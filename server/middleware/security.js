9da1a49fa519fb78055d1c3fd00d0225cd3c512c
Laura Carvajal <laura.carvajal@gmail.com>
2017-11-26 10:06:05 +0000

    /

--- server/middleware/security.js
+++ server/middleware/security.js
@@ -1,6 +1,8 @@
-
-
 module.exports = (req, res, next) => {
-	// TODO api-key
-	next();
+	if (req.headers.api_key === process.env.API_KEY) {
+		return next();
+	}
+	else {
+		return res.status(400).end('Wrong API key');
+	}
 };
