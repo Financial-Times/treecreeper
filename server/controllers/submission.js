fb0e142b166858873d7df2b7b6c1898082a42c1a
Laura Carvajal <laura.carvajal@gmail.com>
2018-02-08 11:27:29 +0000

    contracts fix  🐿 v2.7.0

--- server/controllers/submission.js
+++ server/controllers/submission.js
@@ -16,8 +16,6 @@ const create = async (req, res) => {
 
 	for (let answer of req.body.answers) {
 		crud.create(res, 'SubmissionAnswer', 'id', answer.id, answer, [
-			// {name:'HAS', from: 'Submission', fromId: req.body.node.id, toId: answer.id, to: 'SubmissionAnswer'},
-			// {name:'ANSWERS_QUESTION', from: 'SubmissionAnswer', fromId: answer.id, toId: answer.questionId, to: 'SurveyQuestion'}
 			{name:'HAS', from: 'Submission', fromUniqueAttrName: 'id', fromUniqueAttrValue: req.body.node.id, toUniqueAttrName: 'id', toUniqueAttrValue: answer.id, to: 'SubmissionAnswer'},
 			{name:'ANSWERS_QUESTION', from: 'SubmissionAnswer', fromUniqueAttrName: 'id', fromUniqueAttrValue: answer.id, toUniqueAttrName: 'id', toUniqueAttrValue: answer.questionId, to: 'SurveyQuestion'}
 
