ad210a458d901b04fa0de9ba63ba88f042aa9b87
tejathomas <teja.thomas@ft.com>
2018-02-19 17:11:45 +0000

    Copy answers from previous surveys to new contracts on login

--- server/controllers/supplier.js
+++ server/controllers/supplier.js
@@ -3,6 +3,126 @@ const util = require('util');
 
 const stringify = (object) => util.inspect(object, { showHidden: false, depth: null, colors: false, breakLength: Infinity });
 
+const getSupplierDiligenceWithoutSubmissions = async (supplierId, surveyId) => {
+	const submissions = [];
+	const subsWithoutAnswersQuery = `MATCH (Supplier {id:"${supplierId}"})
+									-[:SIGNS]->(Contract)
+									-[r:SUBMITS]->(subs:Submission) 
+									WHERE NOT (subs)-[:HAS]->() 
+									AND subs.surveyId IN ["${surveyId}"]
+									RETURN subs`;
+	const submissionsWithoutAnswers = await db.run(subsWithoutAnswersQuery); 
+	if (submissionsWithoutAnswers.records.length){
+		submissionsWithoutAnswers.records.forEach(submission => {
+			submissions.push(submission._fields[0].properties.id);
+		});
+		console.log('submissions', submissions);
+		return submissions;
+	}
+	return null;
+};
+
+const findValidSupplierDiligenceSubmission = async (supplierId, surveyId) => {
+	let timeSinceLastYear = new Date(new Date().setFullYear(new Date().getFullYear() - 1)).valueOf();
+	const subsWithAnswersQuery = `MATCH submissions=(Supplier {id:"${supplierId}"})
+									-[:SIGNS]->(Contract)
+									-[:SUBMITS]->(submission:Submission {surveyId: "${surveyId}"})
+									-[:HAS]->(SubmissionAnswer)
+									-[:ANSWERS_QUESTION]->(x: SurveyQuestion) 
+									WHERE submission.status IN ['submitted', 'resubmitted'] 
+									AND submission.submittedDate > ${timeSinceLastYear}
+									RETURN submissions ORDER BY submission.submittedDate desc`;
+	const subsWithAnswers = await db.run(subsWithAnswersQuery);
+	let submissionObj = {};
+	if (subsWithAnswers.records.length) {
+		for (const record of subsWithAnswers.records) {
+			for (const field of record._fields) {
+				let submission;
+				let submissionAnswer;
+				let surveyQuestion;
+				for (const segment of field.segments) {
+					switch(segment.relationship.type) {
+						case 'SUBMITS':
+							submission = submission || segment.end.properties;
+							submissionObj.status = submission.status;
+							submissionObj.id = submission.id;
+						break;
+						case 'HAS':
+							submissionAnswer = submissionAnswer || segment.end.properties;
+
+							if (!submissionObj[submissionAnswer.questionId]) {
+								submissionObj[submissionAnswer.questionId] = {
+									answer: submissionAnswer.value,
+									type: submissionAnswer.type,
+								};
+							}
+							else {
+								submissionObj[submissionAnswer.questionId].answer = submissionAnswer.value;
+							}
+						break;
+						case 'ANSWERS_QUESTION':
+							surveyQuestion = surveyQuestion || segment.end.properties;
+
+							if (!submissionObj[surveyQuestion.id]) {
+								submissionObj[surveyQuestion.id] = {
+									question: surveyQuestion.text,
+								};
+							}
+							else {
+								submissionObj[surveyQuestion.id].question = surveyQuestion.text;
+							}
+						break;
+					}
+				}
+			}
+		}
+	}
+	return submissionObj;
+};
+
+const cloneSupplierDiligenceSubmission = async (supplierId, surveyId) => {
+	const submissionsToClone = await getSupplierDiligenceWithoutSubmissions(supplierId, surveyId);
+	if (submissionsToClone) {
+		const previousSubmission = await findValidSupplierDiligenceSubmission(supplierId, surveyId);
+		console.log('previousSubmission',previousSubmission);
+		if(Object.values(previousSubmission).length > 0) {
+			let submissionCloneQuery = '';
+			//MATCH submissions
+			submissionsToClone.forEach( (submissionId, submissionIndex) => {
+				submissionCloneQuery += submissionIndex === 0 ? 'MATCH ' : '';
+				submissionCloneQuery += `(submission${submissionIndex}:Submission {id: '${submissionId}'}),` ;
+			});
+			//MATCH questions
+			Object.keys(previousSubmission).forEach((key, questionIndex) => {
+				if(previousSubmission[key].question){
+					submissionCloneQuery += `(question${questionIndex}:SurveyQuestion {id : '${key}'}),`;
+				}
+			});
+			submissionCloneQuery = submissionCloneQuery.slice(0, -1);
+			submissionCloneQuery += 'CREATE ';
+			//CREATE answers and relationship to survey questions
+			submissionsToClone.forEach((submissionId, submissionIndex) => {	
+				Object.keys(previousSubmission).forEach((key,answerIndex) => {
+				if (previousSubmission[key].question){
+					const answer = {
+						id: key + submissionId,
+						questionId: key,
+						type: previousSubmission[key].type,
+						value: previousSubmission[key].answer
+					};
+					submissionCloneQuery += `(submission${submissionIndex}) -[:HAS]-> (:SubmissionAnswer ${stringify(answer)}) -[:ANSWERS_QUESTION]-> (question${answerIndex}),`;	
+				}	
+				});
+			});
+			submissionCloneQuery = submissionCloneQuery.slice(0, -1);	
+			console.log('submissionQuery', submissionCloneQuery);
+			const response = await db.run(submissionCloneQuery);
+			return response;
+		}
+	}
+	return null;
+};
+
 //new create function - creates supplier, company info submission, all contracts and submissions for each diligence type of contract
 const create = async (req, res) => {
 	const supplierNode = 'supplierNode';
@@ -21,7 +141,6 @@ const create = async (req, res) => {
 										WITH ${supplierNode}, ${companyInfoNode} 
 										MATCH (sur:Survey {id: 'company-info'}) 
 										MERGE (${companyInfoNode})-[:ANSWERS]-> (sur)`;
-	let contractCount = req.body.contracts.length;
 	req.body.contracts.forEach((contract, contractIndex) => {
 		const {dt} = contract;
 		const contractDetails = contract;
@@ -34,108 +153,29 @@ const create = async (req, res) => {
 				id: survey.id + contract.id,
 				surveyId: survey.id,
 				contractId: contract.id,
-				submittedDate: Date.now()
 			};	
 			const submissionNode = `${contractNode}sub${surveyIndex}`;
 			createQuery += ` MERGE (${contractNode}) -[:SUBMITS]-> (${submissionNode}:Submission ${stringify(submission)})
 											ON CREATE SET ${submissionNode}.status = 'pending'
 											WITH ${submissionNode}, ${contractNode}, ${supplierNode}
-										 	MATCH (sur: Survey {id: '${survey.id}'})
-										 	MERGE (${submissionNode})-[:ANSWERS]-> (sur)`;
+											MATCH (sur: Survey {id: '${survey.id}'})
+											MERGE (${submissionNode})-[:ANSWERS]-> (sur)`;
 		});
 	});
-	console.log('query', createQuery);
 	try{
 		const result = await db.run(createQuery);
-		//check for existing submissions
-		findValidSupplierSubmissions(req.body.node.id, 'as');
 		console.log('result', result);
-		res.send(result);
+		const supplierDiligence = ['as', 'abc', 'bcm', 'pci'];
+		supplierDiligence.forEach(async (surveyId) => {
+			const cloneResult = await cloneSupplierDiligenceSubmission(req.body.node.id, surveyId);
+			console.log('cloneResult', cloneResult);
+		});
+		res.status(200).end(result);
 	}
 	catch (e) {
 		console.log('initialising database failed', e.toString());
 		return res.status(400).end(e.toString());
 	}
-	
 };
 
-const findValidSupplierSubmissions = async (supplierId, surveyId) => {
-	const subsWithAnswersQuery = `MATCH submissions=(Supplier {id:"${supplierId}"})
-									-[:SIGNS]->(Contract)
-									-[:SUBMITS]->(submission:Submission {surveyId: "${surveyId}"})
-									-[:HAS]->(SubmissionAnswer)
-									-[:ANSWERS_QUESTION]->(x: SurveyQuestion) 
-									RETURN submissions ORDER BY x.id`;
-	const subsWithAnswers = await db.run(subsWithAnswersQuery);
-	const subsWithoutAnswersQuery = `MATCH (Supplier {id:"${supplierId}"})
-									-[:SIGNS]->(Contract)
-									-[r:SUBMITS]->(subs:Submission {surveyId: "${surveyId}"}) 
-									WHERE NOT (subs)-[:HAS]->() 
-									RETURN subs`;
-	const subsWithOutAnswers = await db.run(subsWithoutAnswersQuery); //Contract 2, AS submission
-	let submissionObj = {};
-
-		if (subsWithAnswers.records.length) {
-			for (const record of subsWithAnswers.records) {
-				for (const field of record._fields) {
-					let submission;
-					let submissionAnswer;
-					let surveyQuestion;
-
-					for (const segment of field.segments) {
-
-						switch(segment.relationship.type) {
-							case 'SUBMITS':
-								submission = submission || segment.end.properties;
-								submissionObj.status = submission.status;
-								submissionObj.id = submission.id;
-							break;
-							case 'HAS':
-								submissionAnswer = submissionAnswer || segment.end.properties;
-
-								if (!submissionObj[submissionAnswer.questionId]) {
-									submissionObj[submissionAnswer.questionId] = {
-										answer: submissionAnswer.value,
-										type: submissionAnswer.type,
-									};
-								}
-								else {
-									submissionObj[submissionAnswer.questionId].answer = submissionAnswer.value;
-								}
-							break;
-							case 'ANSWERS_QUESTION':
-								surveyQuestion = surveyQuestion || segment.end.properties;
-
-								if (!submissionObj[surveyQuestion.id]) {
-									submissionObj[surveyQuestion.id] = {
-										question: surveyQuestion.text,
-									};
-								}
-								else {
-									submissionObj[surveyQuestion.id].question = surveyQuestion.text;
-								}
-							break;
-						}
-					}
-				}
-			}
-			console.log('submissionObj', submissionObj);
-			let submissionQuery = `MATCH (submission:Submission {id:'asaB4L00000008TmeKAE'})`;
-			Object.keys(submissionObj).forEach((key,index) => {
-				const answer = {
-					id: key + 'asaB4L00000008TmeKAE',
-					questionId: key,
-					type: submissionObj[key].type,
-					value: submissionObj[key].answer
-				}
-				if (submissionObj[key].question && submissionObj[key].answer){
-					submissionQuery += ` , (question${index}:SurveyQuestion {id : '${key}'}) WITH submission, question${index} 
-															CREATE (submission) -[:HAS]-> (:SubmissionAnswer ${stringify(answer)}) -[:ANSWERS_QUESTION]-> (question${index})`	
-				}	
-			});
-			console.log('submissionQuery', submissionQuery);
-			const response = await db.run(submissionQuery);
-			console.log(response);
-	}
-}
 module.exports = { create };
