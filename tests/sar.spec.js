32f13e2e9bb2f4a0e531e627f8392466aa4af4f6
dndhm <dneedham@gmail.com>
2018-02-22 12:43:27 +0000

    Add getWithSources method to sar controller  🐿 v2.7.0

--- tests/sar.spec.js
+++ tests/sar.spec.js
@@ -1,14 +1,102 @@
-const app = require('../server/app.js');
+const proxyquire = require('proxyquire');
 const request = require('supertest');
+const { stub } = require('sinon');
+const { expect } = require('chai');
+
+const app = require('../server/app');
+
+const dbRunStub = stub();
+const { getWithSources } = proxyquire('../server/controllers/sar', {
+	'../db-connection': {
+		run: dbRunStub,
+	},
+});
 
 describe('SAR', () => {
 	describe('POST', (done) => {
 		it('has status code 200', () => {
 			request(app)
-			.post('/api/sar')
-			.set('API_KEY', `${process.env.API_KEY}`)
-			.send({sar: 'this is my sar'})
-			.expect(200, done);
+				.post('/api/sar')
+				.set('API_KEY', `${process.env.API_KEY}`)
+				.send({sar: 'this is my sar'})
+				.expect(200, done);
+		});
+	});
+
+	describe('getWithSources', () => {
+		it('should return sar object with sources array', async () => {
+			const sendMock = stub();
+			const reqId = 'someId';
+			const res = {
+				send: sendMock,
+			};
+			const req = {
+				params: {
+					id: reqId,
+				},
+			};
+
+			const sarProperties = {
+				a: 'a',
+				b: 'b',
+				c: 'c',
+			};
+
+			const sourcesProperties = [
+				{
+					x: 'x',
+					y: 'y',
+					z: 'z',
+				},
+				{
+					yes: 'yes',
+					no: 'no',
+					maybe: 'maybe',
+				},
+			];
+
+			dbRunStub
+				.withArgs(`MATCH (sar { id: "${reqId}" }) RETURN sar`)
+				.resolves({
+					records: [
+						{
+							_fields: [
+								{
+									properties: sarProperties,
+								},
+							],
+						},
+					],
+				});
+
+			dbRunStub
+				.withArgs(`MATCH ({ id: "${reqId}" })-[:CONSUMES]->(sources) RETURN sources`)
+				.resolves({
+					records: [
+						{
+							_fields: [
+								{
+									properties: sourcesProperties[0],
+								},
+							],
+						},
+						{
+							_fields: [
+								{
+									properties: sourcesProperties[1],
+								},
+							],
+						},
+					],
+				});
+
+			const expected = Object.assign({}, sarProperties, {
+				sources: sourcesProperties,
+			});
+
+			await getWithSources(req, res);
+
+			expect(sendMock.calledWith(JSON.stringify(expected))).to.be.true;
 		});
 	});
 });
