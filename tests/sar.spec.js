bfe2f72d5236e8ace4d3b68e12550bad9921ade1
katiekoschland <katie.koschland@ft.com>
2018-02-26 09:54:12 +0000

    Unit test - on post of SAR object, source node/array should be created  🐿 v2.7.0

--- tests/sar.spec.js
+++ tests/sar.spec.js
@@ -2,6 +2,7 @@ const proxyquire = require('proxyquire');
 const request = require('supertest');
 const { stub } = require('sinon');
 const { expect } = require('chai');
+const { isEqual} = require('lodash/fp');
 
 const app = require('../server/app');
 
@@ -20,106 +21,140 @@ const sources = [{
 }];
 
 const deleteSource = async () => request(app)
-		.delete('/api/Source/id/livefyre_customerEmail@test.com_1519207670717')
-		.set('API_KEY', `${process.env.API_KEY}`)
-		.send({ mode: 'detach' });
+.delete('/api/Source/id/livefyre_customerEmail@test.com_1519207670717')
+.set('API_KEY', `${process.env.API_KEY}`)
+.send({ mode: 'detach' });
 
 const deleteSar = async () => request(app)
-		.delete('/api/SAR/id/customerEmail@test.com_1519207670717')
-		.set('API_KEY', `${process.env.API_KEY}`)
-		.send({ mode: 'detach' });
+.delete('/api/SAR/id/customerEmail@test.com_1519207670717')
+.set('API_KEY', `${process.env.API_KEY}`)
+.send({ mode: 'detach' });
 
 describe('SAR', () => {
 	describe('POST', () => {
-
 		after(() =>
 		deleteSar()
 		.then(() =>
 		deleteSource()));
 
-	it('has status code 200', (done) => {
-		request(app)
-		.post('/api/sar')
-		.set('API_KEY', `${process.env.API_KEY}`)
-		.send({ sar, sources })
-		.expect(200, done);
+		it('has status code 200', (done) => {
+			request(app)
+			.post('/api/sar')
+			.set('API_KEY', `${process.env.API_KEY}`)
+			.send({ sar, sources })
+			.expect(200, done);
+		});
+
+		describe('Source node should be created', () => {
+			before(() => {
+				request(app)
+				.post('/api/sar')
+				.set('API_KEY', `${process.env.API_KEY}`)
+				.send({ sar, sources });
+			});
+
+			after(() =>
+			deleteSar()
+			.then(() =>
+			deleteSource()));
+
+			it('expect it to be included within the SAR object', (done) => {
+				const expected = {
+					'id':'customerEmail@test.com_1519207670717',
+					'sources':[
+						{
+							'name':'livefyre',
+							'id':'livefyre_customerEmail@test.com_1519207670717',
+							'status':'pending'
+						}
+					]
+				};
+
+				request(app)
+				.get('/api/sar/customerEmail@test.com_1519207670717')
+				.set('API_KEY', `${process.env.API_KEY}`)
+				.then( (response) => {
+					expect(response.res.text).to.equal(JSON.stringify(expected));
+				})
+				.end(done());
+			});
+		});
 	});
-});
 
-describe('getWithSources', () => {
-	it('should return sar object with sources array', async () => {
-		const sendMock = stub();
-		const reqId = 'someId';
-		const res = {
-			send: sendMock,
-		};
-		const req = {
-			params: {
-				id: reqId,
-			},
-		};
-
-		const sarProperties = {
-			a: 'a',
-			b: 'b',
-			c: 'c',
-		};
-
-		const sourcesProperties = [
-			{
-				x: 'x',
-				y: 'y',
-				z: 'z',
-			},
-			{
-				yes: 'yes',
-				no: 'no',
-				maybe: 'maybe',
-			},
-		];
-
-		dbRunStub
-		.withArgs(`MATCH (sar { id: "${reqId}" }) RETURN sar`)
-		.resolves({
-			records: [
-				{
-					_fields: [
-						{
-							properties: sarProperties,
-						},
-					],
+	describe('getWithSources', () => {
+		it('should return sar object with sources array', async () => {
+			const sendMock = stub();
+			const reqId = 'someId';
+			const res = {
+				send: sendMock,
+			};
+			const req = {
+				params: {
+					id: reqId,
 				},
-			],
-		});
+			};
 
-		dbRunStub
-		.withArgs(`MATCH ({ id: "${reqId}" })-[:CONSUMES]->(sources) RETURN sources`)
-		.resolves({
-			records: [
+			const sarProperties = {
+				a: 'a',
+				b: 'b',
+				c: 'c',
+			};
+
+			const sourcesProperties = [
 				{
-					_fields: [
-						{
-							properties: sourcesProperties[0],
-						},
-					],
+					x: 'x',
+					y: 'y',
+					z: 'z',
 				},
 				{
-					_fields: [
-						{
-							properties: sourcesProperties[1],
-						},
-					],
+					yes: 'yes',
+					no: 'no',
+					maybe: 'maybe',
 				},
-			],
+			];
+
+			dbRunStub
+			.withArgs(`MATCH (sar { id: "${reqId}" }) RETURN sar`)
+			.resolves({
+				records: [
+					{
+						_fields: [
+							{
+								properties: sarProperties,
+							},
+						],
+					},
+				],
+			});
+
+			dbRunStub
+			.withArgs(`MATCH ({ id: "${reqId}" })-[:CONSUMES]->(sources) RETURN sources`)
+			.resolves({
+				records: [
+					{
+						_fields: [
+							{
+								properties: sourcesProperties[0],
+							},
+						],
+					},
+					{
+						_fields: [
+							{
+								properties: sourcesProperties[1],
+							},
+						],
+					},
+				],
+			});
+
+			const expected = Object.assign({}, sarProperties, {
+				sources: sourcesProperties,
+			});
+
+			await getWithSources(req, res);
+
+			expect(sendMock.calledWith(JSON.stringify(expected))).to.be.true;
 		});
-
-		const expected = Object.assign({}, sarProperties, {
-			sources: sourcesProperties,
-		});
-
-		await getWithSources(req, res);
-
-		expect(sendMock.calledWith(JSON.stringify(expected))).to.be.true;
 	});
-});
 });
