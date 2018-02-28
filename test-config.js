9b910bbbcf67c5adc8cdffb686ed193f56bb5971
robinmarr-ft <robin.marr@ft.com>
2018-02-28 17:28:47 +0000

    add sinon-chai to unit tests

--- test-config.js
+++ test-config.js
@@ -1,3 +1,8 @@
+const chai = require('chai');
+const sinonChai = require('sinon-chai');
+
+chai.use(sinonChai);
+
 require('babel-polyfill');
 require('babel-core/register')({
 	presets: [require.resolve('babel-preset-env')],
