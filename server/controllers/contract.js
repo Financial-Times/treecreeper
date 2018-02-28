13716b916b7d839135d278d5f58df5dde60dab41
robinmarr-ft <robin.marr@ft.com>
2018-02-28 17:21:06 +0000

    refactor the contract get controller

--- server/controllers/contract.js
+++ server/controllers/contract.js
@@ -1,5 +1,8 @@
+const { curry } = require('lodash');
+
 const crud = require('./_crud');
 const db = require('../db-connection');
+const cypher = require('../lib/cypher');
 
 const getAllforOne = async (req, res) => {
 	return crud.getAllforOne(res, {name:'SIGNS', from: 'Supplier', to: 'Contract'}, req.params.supplierId);
@@ -7,139 +10,52 @@ const getAllforOne = async (req, res) => {
 
 const get = async (req, res) => {
 	try {
+		const { supplierId } = req.params;
+		if(!supplierId) return res.status(404).end('a supplier id is required.');
 
-		// TODO replace this while thing with _cypher-to-json.js
+		const query = `MATCH (supplier:Supplier {id: '${supplierId}'})
+			OPTIONAL MATCH (supplier)-[:SUBMITS]->(topLevel:Submission)
+			WITH supplier, topLevel
+			OPTIONAL MATCH (topLevel)-[:HAS|:ANSWERS]->(topLevelAnswer:SubmissionAnswer)-[:ANSWERS_QUESTION]->(topLevelQuestion:SurveyQuestion)
+			OPTIONAL MATCH (supplier)-[:SIGNS]->(contract:Contract)-[:SUBMITS]->(submission:Submission)
+			WITH supplier, topLevel, topLevelAnswer, topLevelQuestion, contract, submission
+			OPTIONAL MATCH (submission)-[:HAS|:ANSWERS]->(answer:SubmissionAnswer)-[:ANSWERS_QUESTION]->(question:SurveyQuestion)
+			RETURN supplier, topLevel, topLevelAnswer, topLevelQuestion, contract, submission, question, answer ORDER BY question.id`;
 
-		const query = `MATCH p=(:Supplier {id:'${req.params.supplierId}'})-[:SIGNS*0..]->()-[r:SUBMITS*0..]->()-[:HAS|:ANSWERS*0..]->()-[:ANSWERS_QUESTION*0..]->(x:SurveyQuestion) RETURN p ORDER BY x.id`;
-		// console.log('[CONTRACT]', query);
 		const result = await db.run(query);
+		const records = cypher.res.parse(result.records);
+		const [ supplier ] = cypher.res.uniquePropertiesByKey('supplier', records);
 
-		const contractsObj = {
-			//contractId: {
-			// 	  ...
-			//    submissions : {
-			//		submissionId: {
-			//			...
-			//			answers: {
-			//				...
-			//				answerId: {
-			//					"id": answerId,
-            //					"text": "No",
-            //					"questionText": ""
-			//				}
-			//			}
-			//	    }
-			//    }
-			// }
-		};
+		const questions = cypher.res.uniquePropertiesByKey('question', records)
+			.map(formatQuestion);
 
-		const supplierObj = {
-			submissions: {}
-		};
+		const answers = cypher.res.uniquePropertiesByKey('answer', records)
+			.map(mergeQuestionsAndAnswers(questions));
 
-		if (result.records.length) {
-			for (const record of result.records) {
-				for (const field of record._fields) {
-					let contract;
-					let submission;
-					let answer;
-					let question;
-					let survey;
+		const submissions = cypher.res.uniquePropertiesByKey('submission', records)
+			.map(addAnswersToSubmission(answers));
 
-					for (const segment of field.segments) {
+		const contracts = cypher.res.uniquePropertiesByKey('contract', records)
+			.map(addSubmissionsToContract(submissions));
 
-						switch(segment.relationship.type) {
-							case 'SIGNS':
-								contract = contract || segment.end.properties;
+		const response = [ contracts, supplier ];
 
-								if (!contractsObj[contract.id]) {
-									contractsObj[contract.id] = contract;
-								}
-							break;
-
-							case 'SUBMITS':
-								contract = contract || segment.start.properties;
-								submission = submission || segment.end.properties;
-
-								if (submission.type === 'topLevel') {
-									if (!supplierObj.submissions[submission.id]) {
-										supplierObj.submissions[submission.id] = submission;
-									}
-								}
-								else if (!contractsObj[contract.id]) {
-									contractsObj[contract.id] = contract;
-								}
-								else {
-									if (!contractsObj[contract.id].submissions) {
-										contractsObj[contract.id].submissions = {};
-									}
-									if (!contractsObj[contract.id].submissions[submission.id]) {
-										contractsObj[contract.id].submissions[submission.id] = submission;
-									}
-								}
-							break;
-
-							case 'HAS':
-								answer = answer || segment.end.properties;
-								submission = submission || segment.start.submission;
-
-								if (submission.type === 'topLevel') {
-									if (!supplierObj.submissions[submission.id].answers) {
-										supplierObj.submissions[submission.id].answers = {};
-									}
-									supplierObj.submissions[submission.id].answers[answer.id] = answer;
-								}
-								else {
-
-									if (!contractsObj[contract.id].submissions[submission.id].answers) {
-										contractsObj[contract.id].submissions[submission.id].answers = {};
-									}
-									contractsObj[contract.id].submissions[submission.id].answers[answer.id] = answer;
-								}
-
-							break;
-
-							case 'ANSWERS_QUESTION':
-								answer = answer || segment.start.properties;
-								question = question || segment.end.properties;
-
-								if (submission.type === 'topLevel') {
-									supplierObj.submissions[submission.id].answers[answer.id].questionText = question.text;
-								}
-								else {
-									contractsObj[contract.id].submissions[submission.id].answers[answer.id].questionText = question.text;
-								}
-							break;
-
-							case 'ANSWERS':
-								if (submission.type === 'topLevel') {
-									survey = survey || segment.end.properties;
-									supplierObj.submissions[submission.id].surveyId = survey.id;
-								}
-								else {
-									contract = contract || segment.start.properties;
-									survey = survey || segment.end.properties;
-									contractsObj[contract.id].submissions[submission.id].surveyId = survey.id;
-								}
-							break;
-						}
-					}
-				}
-			}
-		}
-
-		const isEmpty = !Object.keys(contractsObj).length;
-
-		if (isEmpty) {
-			const message =	`No submissions found for ${req.params.supplierId}`;
+		if (!contracts.length) {
+			const message =	`No submissions found for ${supplierId}`;
 			return res.status(404).end(message);
 		}
-		return res.send([contractsObj, supplierObj]);
-	}
-	catch (e) {
+		return res.send(response);
+	} catch (e) {
 		console.log('[CONTRACT]', e.toString());
 		return res.status(500).end(e.toString());
 	}
 };
+
+const formatQuestion = ({ fieldType, text, id }) => ({ questionId: id, fieldType, text });
+const mergeQuestionsAndAnswers = curry((questions, answer) => Object.assign({}, answer, questions.find((question) => answer.questionId === question.questionId)));
+const addAnswersToSubmission = curry((answers, submission) => Object.assign(submission, {
+	answers: answers.filter(answer => (answer.id.startsWith(submission.surveyId) && answer.id.endsWith(submission.contractId)))
+}));
+const addSubmissionsToContract = curry((submissions, contract) => Object.assign(contract, { submissions: submissions.filter(submission => submission.contractId === contract.id)}));
 
 module.exports = { get, getAllforOne };
