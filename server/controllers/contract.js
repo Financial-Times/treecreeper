eeb3b7a0b3fcdc4dbf27376ed2064f7fe137b0cc
Bhargava Guda <bhargava.guda@ft.com>
2018-03-15 16:11:52 +0000

    Email field added to the surveys

--- server/controllers/contract.js
+++ server/controllers/contract.js
@@ -7,7 +7,7 @@ const getAllforOne = async (req, res) => {
 
 const get = async (req, res) => {
 	try {
-
+console.log("hjhjhjhjhjhjhjhjhjhjh")
 		// TODO replace this while thing with _cypher-to-json.js
 
 		const query = `MATCH p=(:Supplier {id:'${req.params.supplierId}'})-[:SIGNS*0..]->()-[r:SUBMITS*0..]->()-[:HAS|:ANSWERS*0..]->()-[:ANSWERS_QUESTION*0..]->(x:SurveyQuestion) RETURN p ORDER BY x.id`;
