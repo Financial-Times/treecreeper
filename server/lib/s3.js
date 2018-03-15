8f4658f1bbb8bef0c7b98129e32ab03c57d54e3a
robinmarr-ft <robin.marr@ft.com>
2018-03-15 10:24:09 +0000

    add a mock getSignedURL for development

--- server/lib/s3.js
+++ server/lib/s3.js
@@ -1,4 +1,5 @@
 const { S3 } = require('aws-sdk');
+const { join } = require('path');
 
 const {
 	AWS_API_VERSION: apiVersion = '2006-03-01',
@@ -7,12 +8,21 @@ const {
 	AWS_EXPIRES: Expires = 60 * 60 * 24, // 24 hours
 	AWS_ACCESS_KEY_ID: accessKeyId,
 	AWS_SECRET_ACCESS_KEY: secretAccessKey,
+	NODE_ENV: env = 'development'
 } = process.env;
 
+
+const getSignedUrlMock = (action, { Bucket, Key }, callback) => {
+	callback(null, join('/uploads', Bucket, Key));
+};
+
+const s3Client = () => {
+	if(env === 'development') return { getSignedUrl: getSignedUrlMock };
+	else return new S3({apiVersion, region, accessKeyId, secretAccessKey});
+};
+
 exports.getSignedUrl = Key => new Promise((resolve, reject) => {
-	const client = new S3({ apiVersion, region, accessKeyId, secretAccessKey});
-	const params = { Bucket, Key, Expires };
-	client.getSignedUrl('getObject', params, (error, url) => {
+	s3Client().getSignedUrl('getObject', { Bucket, Key, Expires }, (error, url) => {
 		if (url) resolve(url);
 		else reject(error);
 	});
