495ddcf8989617a3ddf48f97518618b4b8db5705
geoff thorpe <geoff.thorpe@ft.com>
2018-03-16 13:18:46 +0000

    Ingtroduce constaints on CMDB types  🐿 v2.7.0

--- scripts/init.js
+++ scripts/init.js
@@ -14,7 +14,24 @@ const constraints = async (verb) => {
 		`${verb} CONSTRAINT ON (r:Submission) ASSERT exists(r.id)`,
 		`${verb} CONSTRAINT ON (s:Survey) ASSERT s.id IS UNIQUE`,
 		`${verb} CONSTRAINT ON (s:Survey) ASSERT exists(s.id)`,
-		`${verb} CONSTRAINT ON (s:Survey) ASSERT exists(s.version)`
+		`${verb} CONSTRAINT ON (s:Survey) ASSERT exists(s.version)`,
+		// cmdb constainsts
+		`${verb} CONSTRAINT ON (s:Domain) ASSERT s.id IS UNIQUE`,
+        `${verb} CONSTRAINT ON (s:Domain) ASSERT exists(s.id)`,
+        `${verb} CONSTRAINT ON (c:Endpoint) ASSERT c.id IS UNIQUE`,
+        `${verb} CONSTRAINT ON (c:Endpoint) ASSERT exists(c.id)`,
+        `${verb} CONSTRAINT ON (r:Group) ASSERT r.id IS UNIQUE`,
+        `${verb} CONSTRAINT ON (r:Group) ASSERT exists(r.id)`,
+        `${verb} CONSTRAINT ON (s:Person) ASSERT s.id IS UNIQUE`,
+        `${verb} CONSTRAINT ON (s:Person) ASSERT exists(s.id)`,
+        `${verb} CONSTRAINT ON (s:Product) ASSERT s.id IS UNIQUE`,
+        `${verb} CONSTRAINT ON (s:Product) ASSERT exists(s.id)`,
+        `${verb} CONSTRAINT ON (s:Repository) ASSERT s.id IS UNIQUE`,
+        `${verb} CONSTRAINT ON (s:Repository) ASSERT exists(s.id)`,
+        `${verb} CONSTRAINT ON (s:System) ASSERT s.id IS UNIQUE`,
+        `${verb} CONSTRAINT ON (s:System) ASSERT exists(s.id)`,
+        `${verb} CONSTRAINT ON (s:Team) ASSERT s.id IS UNIQUE`,
+        `${verb} CONSTRAINT ON (s:Team) ASSERT exists(s.id)`
 	];
 
 	const setupConstraintIfPossible = (constraintQuery) => db.run(constraintQuery).catch(() => Promise.resolve());
