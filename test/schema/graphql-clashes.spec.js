1d727ee5fd8f7ee62dd80d21f6202e3f7e9b6e4d
Rhys Evans <wheresrhys@gmail.com>
2018-07-19 13:52:54 +0100

    add product -> product dependencies, hidden from graphQL (#216)
    * add product -> product dependencies, hidden from graphQL
     🐿 v2.10.0
    
    * lint
     🐿 v2.10.0

--- test/schema/graphql-clashes.spec.js
+++ test/schema/graphql-clashes.spec.js
@@ -7,9 +7,11 @@ describe('(lack of) graphql clashes', () => {
 			it('has no graphql property name clashes', () => {
 				if (relationshipsSchema[type.name]) {
 					const ownProperties = Object.keys(type.properties);
-					const relationshipProperties = relationshipsSchema[type.name].map(
-						def => def.name
-					);
+					const relationshipProperties = relationshipsSchema[type.name]
+						.map(def => def.name)
+						// allow for relationships that are not exposed in graphQL
+						.filter(name => !!name);
+
 					expect(
 						[...new Set([...ownProperties, ...relationshipProperties])].length
 					).to.equal(ownProperties.concat(relationshipProperties).length);
