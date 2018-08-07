40571e89b8492af2e9a2e4173a9211ac33f7ed30
katiekoschland <katie.koschland@ft.com>
2018-08-07 08:57:53 +0100

    Refactored so that it generates data only if the file doesn't exist  🐿 v2.10.0

--- scripts/load-testing/lib/generate/index.js
+++ scripts/load-testing/lib/generate/index.js
@@ -3,22 +3,28 @@ const generateSystemData = require('./system');
 const generateGroupData = require('./group');
 const generateTeamData = require('./team');
 
-const exportToCSV = () => {
-	[generateSystemData(), generateGroupData(), generateTeamData()].map(array => {
-		const header = Object.keys(array[0]).join(',');
-		const data = array.map(object => {
-			return Object.values(object).join(',');
-		});
-		const dataToCSV = [header, ...data].join('\n');
-		const nodeType = array[0].primaryNode.toLowerCase();
-		const fileName = `scripts/load-testing/lib/exportToCSV/${nodeType}.csv`;
-		fs.existsSync(fileName)
-			? console.log('file already exists')
-			: fs.writeFile(fileName, dataToCSV, err => {
-					if (err) throw err;
-			  });
-	});
+const generateData = {
+	System: generateSystemData(),
+	Group: generateGroupData(),
+	Team: generateTeamData()
 };
 
+const exportToCSV = () => {
+	['System', 'Group', 'Team'].map(nodeType => {
+		const fileName = `scripts/load-testing/lib/exportToCSV/${nodeType}.csv`;
+		if (fs.existsSync(fileName)) {
+			console.log('file already exists');
+		} else {
+			const array = generateData[nodeType];
+			const header = Object.keys(array[0]).join(',');
+			const data = array.map(object => {
+				return Object.values(object).join(',');
+			});
+			const dataToCSV = [header, ...data].join('\n');
+			fs.writeFileSync(fileName, dataToCSV, err => {
+				if (err) throw err;
+			});
+		}
+	});
+};
 exportToCSV();
-module.exports = exportToCSV;
