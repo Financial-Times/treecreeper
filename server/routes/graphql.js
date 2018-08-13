452ce5cc40f4d9c72505030093edb90787c85fe8
Rhys Evans <rhys.evans@ft.com>
2018-08-13 12:07:25 +0100

    log graphql endpoint  🐿 v2.10.2

--- server/routes/graphql.js
+++ server/routes/graphql.js
@@ -3,14 +3,14 @@ const timeout = require('connect-timeout');
 const security = require('../middleware/security');
 const maintenance = require('../middleware/maintenance');
 const graphql = require('../graphql/controllers');
-const { logger } = require('../lib/request-context');
-
+const { logger, setContext } = require('../lib/request-context');
 const bodyParsers = [
 	bodyParser.json({ limit: '8mb' }),
 	bodyParser.urlencoded({ limit: '8mb', extended: true })
 ];
 
 module.exports = router => {
+	setContext('endpoint', 'graphql');
 	router.use(timeout('65s'));
 	router.use(security.requireApiAuthOrS3o);
 	router.use(maintenance.disableReads);
