44061d8e68ddd0d928795cbf060755ea10fe2ed7
Rhys Evans <rhys.evans@ft.com>
2018-09-04 12:17:38 +0100

    test for get types

--- test/methods/get-types.spec.js
+++ test/methods/get-types.spec.js
@@ -1,5 +1,37 @@
-const getTypes = require('../../').getTypes;
+const { getTypes } = require('../../');
+const getType = require('../../methods/get-type');
+const rawData = require('../../lib/raw-data');
+const sinon = require('sinon');
+const cache = require('../../lib/cache');
 
-describe.skip('get-types', () => {
-	it('is not tested yet', () => {});
+describe('get-types', () => {
+	const sandbox = sinon.createSandbox();
+	beforeEach(() => {
+		sandbox.stub(rawData, 'getTypes').returns([
+			{
+				name: 'Type1'
+			},
+			{ name: 'Type2' }
+		]);
+		sandbox.stub(getType, 'method');
+		getType.method.callsFake(type => ({ name: type, description: 'woo' }));
+	});
+	afterEach(() => {
+		sandbox.restore();
+		cache.clear();
+	});
+	it('gets all types', () => {
+		expect(getTypes({ option: 'value' })).to.eql([
+			{
+				name: 'Type1',
+				description: 'woo'
+			},
+			{
+				name: 'Type2',
+				description: 'woo'
+			}
+		]);
+		expect(getType.method).calledWith('Type1', { option: 'value' });
+		expect(getType.method).calledWith('Type2', { option: 'value' });
+	});
 });
