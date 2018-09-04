d74d7ba1d607a48ea72f1b65811c185e9b50d6fe
Rhys Evans <rhys.evans@ft.com>
2018-09-04 09:11:12 +0100

    graphql defs test

--- index.js
+++ index.js
@@ -3,13 +3,7 @@ module.exports = Object.assign(
 		getType: require('./methods/get-type').method,
 		getTypes: require('./methods/get-types').method,
 		getRelationships: require('./methods/get-relationships').method,
-		// getStringPattern: name => RegExp,
-		// getEnum: enumType => ({}),
 		getEnums: require('./methods/get-enums').method,
-		// getFilterFields: type => [],
-		// getIdentifierFields: type => [],
-		// getGraphQLSchema: () => '',
-
 		graphqlDefs: require('./lib/generate-graphql-defs')
 	},
 	require('./lib/validate')
--- methods/get-enums.js
+++ methods/get-enums.js
@@ -4,26 +4,21 @@ const cache = require('../lib/cache');
 const mapToObjectResolver = keys =>
 	keys.reduce((resolver, key) => Object.assign(resolver, { [key]: key }), {});
 
-module.exports.method = ({ withMeta = false } = {}) => {
-	let enums = cache.get('enums', withMeta);
-	if (!enums) {
-		enums = Object.entries(rawData.getEnums()).reduce(
-			(map, [key, { options, description }]) => {
-				options = Array.isArray(options)
-					? mapToObjectResolver(options)
-					: options;
-				const entry = withMeta
-					? {
-							options,
-							description
-					  }
-					: options;
+module.exports.method = cache.cacheify(({ withMeta = false } = {}) => {
+	return  Object.entries(rawData.getEnums()).reduce(
+		(map, [key, { options, description }]) => {
+			options = Array.isArray(options)
+				? mapToObjectResolver(options)
+				: options;
+			const entry = withMeta
+				? {
+						options,
+						description
+				  }
+				: options;
 
-				return Object.assign(map, { [key]: entry });
-			},
-			{}
-		);
-		cache.set('enums', withMeta, enums);
-	}
-	return enums;
-};
+			return Object.assign(map, { [key]: entry });
+		},
+		{}
+	);
+}, ({withMeta = false} = {}) => `enums: ${withMeta}`);
