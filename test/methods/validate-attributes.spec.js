daab4f266a31d7925eeb60ad449dfaac84f2d210
Rhys Evans <rhys.evans@ft.com>
2018-09-04 14:19:13 +0100

    tests for validation

--- test/methods/validate-attributes.spec.js
+++ test/methods/validate-attributes.spec.js
@@ -1,16 +1,9 @@
 const sinon = require('sinon');
 const getType = require('../../methods/get-type');
+const getEnums = require('../../methods/get-enums');
 const {validateAttributes} = require('../../')
-const getValidatorWithSchema = (types, enums) => {
-	return proxyquire('../../server/crud/schema-compliance', {
-		'../../schema': {
-			typesSchema: [types],
-			enumsSchema: enums || {}
-		}
-	}).validateNodeAttributes;
-};
 
-describe.only('validateAttributes', () => {
+describe('validateAttributes', () => {
 	const sandbox = sinon.createSandbox();
 	beforeEach(() => {
 		sandbox.stub(getType, 'method')
@@ -18,13 +11,13 @@ describe.only('validateAttributes', () => {
 
 	afterEach(() => sandbox.restore())
 	describe('validating strings', () => {
-		before(() => {
+		beforeEach(() => {
 			getType.method.returns({
 				name: 'Thing',
 				properties: {
 					prop: {
 						type: 'String',
-						pattern: /^[^z]+$/ //exclude the letter z
+						validator: /^[^z]+$/ //exclude the letter z
 					}
 				}
 			})
@@ -49,123 +42,113 @@ describe.only('validateAttributes', () => {
 		});
 	});
 	describe('validating booleans', () => {
-		const validator = getValidatorWithSchema({
-			name: 'Thing',
-			properties: {
-				prop: {
-					type: 'Boolean'
+		beforeEach(() => {
+			getType.method.returns({
+				name: 'Thing',
+				properties: {
+					prop: {
+						type: 'Boolean'
+					}
 				}
-			}
+			})
 		});
 
 		it('not accept strings', () => {
 			expect(() =>
-				validator('Thing', { prop: 'I am Tracy Beaker' })
+				validateAttributes('Thing', { prop: 'I am Tracy Beaker' })
 			).to.throw();
 		});
 		it('accept booleans', () => {
-			expect(() => validator('Thing', { prop: true })).not.to.throw();
-			expect(() => validator('Thing', { prop: false })).not.to.throw();
+			expect(() => validateAttributes('Thing', { prop: true })).not.to.throw();
+			expect(() => validateAttributes('Thing', { prop: false })).not.to.throw();
 		});
 		it('not accept floats', () => {
-			expect(() => validator('Thing', { prop: 1.34 })).to.throw();
+			expect(() => validateAttributes('Thing', { prop: 1.34 })).to.throw();
 		});
 		it('not accept integers', () => {
-			expect(() => validator('Thing', { prop: 134 })).to.throw();
+			expect(() => validateAttributes('Thing', { prop: 134 })).to.throw();
 		});
 	});
 	describe('validating floats', () => {
-		const validator = getValidatorWithSchema({
+		beforeEach(() => {
+			getType.method.returns({
 			name: 'Thing',
 			properties: {
 				prop: {
 					type: 'Float'
-				}
-			}
+				}}
+			})
 		});
 
 		it('not accept strings', () => {
 			expect(() =>
-				validator('Thing', { prop: 'I am Tracy Beaker' })
+				validateAttributes('Thing', { prop: 'I am Tracy Beaker' })
 			).to.throw();
 		});
 		it('not accept booleans', () => {
-			expect(() => validator('Thing', { prop: true })).to.throw();
-			expect(() => validator('Thing', { prop: false })).to.throw();
+			expect(() => validateAttributes('Thing', { prop: true })).to.throw();
+			expect(() => validateAttributes('Thing', { prop: false })).to.throw();
 		});
 		it('accept floats', () => {
-			expect(() => validator('Thing', { prop: 1.34 })).not.to.throw();
+			expect(() => validateAttributes('Thing', { prop: 1.34 })).not.to.throw();
 		});
 		it('accept integers', () => {
-			expect(() => validator('Thing', { prop: 134 })).not.to.throw();
+			expect(() => validateAttributes('Thing', { prop: 134 })).not.to.throw();
 		});
 	});
 
 	describe('validating integers', () => {
-		const validator = getValidatorWithSchema({
+		beforeEach(() => {
+			getType.method.returns({
 			name: 'Thing',
 			properties: {
 				prop: {
 					type: 'Int'
-				}
-			}
+				}}
+			})
 		});
 
 		it('not accept strings', () => {
 			expect(() =>
-				validator('Thing', { prop: 'I am Tracy Beaker' })
+				validateAttributes('Thing', { prop: 'I am Tracy Beaker' })
 			).to.throw();
 		});
 		it('not accept booleans', () => {
-			expect(() => validator('Thing', { prop: true })).to.throw();
-			expect(() => validator('Thing', { prop: false })).to.throw();
+			expect(() => validateAttributes('Thing', { prop: true })).to.throw();
+			expect(() => validateAttributes('Thing', { prop: false })).to.throw();
 		});
 		it('not accept floats', () => {
-			expect(() => validator('Thing', { prop: 1.34 })).to.throw();
+			expect(() => validateAttributes('Thing', { prop: 1.34 })).to.throw();
 		});
 		it('accept integers', () => {
-			expect(() => validator('Thing', { prop: 134 })).not.to.throw();
+			expect(() => validateAttributes('Thing', { prop: 134 })).not.to.throw();
 		});
 	});
 	describe('validating enums', () => {
-		const validator = getValidatorWithSchema(
+		beforeEach(() => {
+			getType.method.returns(
 			{
 				name: 'Thing',
 				properties: {
 					prop: {
 						type: 'MyEnum'
-					},
-					prop2: {
-						type: 'MyMappingEnum'
 					}
 				}
-			},
+			});
+			sandbox.stub(getEnums, 'method').returns(
 			{
 				MyEnum: {
-					options: ['kittiwake', 'guillemot']
-				},
-				MyMappingEnum: {
-					options: {
 						bear: 'grylls',
 						ray: 'winstone'
-					}
 				}
-			}
-		);
-		it('accept value defined in a simple enum', () => {
-			expect(() => validator('Thing', { prop: 'kittiwake' })).not.to.throw();
+			})
 		});
-
-		it('not accept value not defined in a simple enum', () => {
-			expect(() => validator('Thing', { prop: 'fulmar' })).to.throw();
-		});
-
 		it('accept value defined in a mapping enum', () => {
-			expect(() => validator('Thing', { prop2: 'grylls' })).not.to.throw();
+			expect(() => validateAttributes('Thing', { prop: 'grylls' })).not.to.throw();
 		});
 
 		it('not accept value not defined in a mapping enum', () => {
-			expect(() => validator('Thing', { prop2: 'ban' })).to.throw();
+			expect(() => validateAttributes('Thing', { prop: 'ban' })).to.throw();
 		});
 	});
 });
