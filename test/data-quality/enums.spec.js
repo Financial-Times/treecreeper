10bba6358bb6558f0c999577a923f566d297d3b5
Rhys Evans <rhys.evans@ft.com>
2018-09-04 09:23:55 +0100

    test for valid regexes

--- test/data-quality/enums.spec.js
+++ test/data-quality/enums.spec.js
@@ -1,3 +1,17 @@
+const rawData = require('../../lib/raw-data');
+const enums = rawData.getEnums();
 describe.skip('data quality: enum spec', () => {
-	it('is not tested yet', () => {});
+	Object.entries(enums).forEach(([name, enumDef]) => {
+		describe(name, () => {
+			it('has a description', () => {
+				expect(enumDef.description).to.be.a.string;
+			})
+			it('has an array of options', () => {})
+			it('has an object of key values', () => {})
+			it('has no numbers in option keys	', () => {})
+		})
+	});
+
+
+
 });
