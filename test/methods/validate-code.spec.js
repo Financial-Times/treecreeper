b6e44ff899589738398f25bee824161e3f625ac8
Rhys Evans <rhys.evans@ft.com>
2018-09-04 14:32:04 +0100

    validate attribute names and type names

--- test/methods/validate-code.spec.js
+++ test/methods/validate-code.spec.js
@@ -6,42 +6,38 @@ const { validateCode } = require('../../');
 describe('validateCode', () => {
 	const sandbox = sinon.createSandbox();
 	beforeEach(() => {
-		sandbox.stub(getType, 'method');
+		sandbox.stub(getType, 'method').returns({
+			name: 'Thing',
+			properties: {
+				code: {
+					type: 'String',
+					validator: /^[^z]+$/ //exclude the letter z
+				}
+			}
+		});
 	});
 
 	afterEach(() => sandbox.restore());
-	describe('validating strings', () => {
-		beforeEach(() => {
-			getType.method.returns({
-				name: 'Thing',
-				properties: {
-					code: {
-						type: 'String',
-						validator: /^[^z]+$/ //exclude the letter z
-					}
-				}
-			});
-		});
-		it('accept strings', () => {
-			expect(() =>
-				validateCode('Thing', 'acceptable')
-			).not.to.throw();
-		});
-		it('not accept booleans', () => {
-			expect(() => validateCode('Thing', true )).to.throw();
-			expect(() => validateCode('Thing', false )).to.throw();
-		});
-		it('not accept floats', () => {
-			expect(() => validateCode('Thing', 1.34 )).to.throw();
-		});
-		it('not accept integers', () => {
-			expect(() => validateCode('Thing', 134 )).to.throw();
-		});
-		it('apply string patterns', () => {
-			expect(() =>
-				validateCode('Thing', 'zo-no')
-			).to.throw();
-		});
+
+	it('accept strings', () => {
+		expect(() =>
+			validateCode('Thing', 'acceptable')
+		).not.to.throw();
+	});
+	it('not accept booleans', () => {
+		expect(() => validateCode('Thing', true )).to.throw();
+		expect(() => validateCode('Thing', false )).to.throw();
+	});
+	it('not accept floats', () => {
+		expect(() => validateCode('Thing', 1.34 )).to.throw();
+	});
+	it('not accept integers', () => {
+		expect(() => validateCode('Thing', 134 )).to.throw();
+	});
+	it('apply string patterns', () => {
+		expect(() =>
+			validateCode('Thing', 'zo-no')
+		).to.throw();
 	});
 
 });
---
+++ test/methods/validate-type-name.spec.js
@@ -0,0 +1,26 @@
+const sinon = require('sinon');
+const getType = require('../../methods/get-type');
+const { validateTypeName } = require('../../');
+
+describe('validateTypeName', () => {
+	const sandbox = sinon.createSandbox();
+	beforeEach(() => {
+		sandbox.stub(getType, 'method')
+	});
+
+	afterEach(() => sandbox.restore());
+	it('accept names in the list', () => {
+		getType.method.returns({
+			name: 'Thing',
+
+		});
+		expect(() =>
+			validateTypeName('Thing')
+		).not.to.throw();
+	});
+	it('reject names not in the list', () => {
+		getType.method.returns();
+		expect(() => validateTypeName('Thingo')).to.throw();
+	});
+
+});
