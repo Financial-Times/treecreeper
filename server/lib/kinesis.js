23c666a314690ca46c48e32a332431df88dda393
Rhys Evans <rhys.evans@ft.com>
2018-10-02 17:18:42 +0100

    try disabling kinesis signature cachinhg  🐿 v2.10.3

--- server/lib/kinesis.js
+++ server/lib/kinesis.js
@@ -28,7 +28,8 @@ function Kinesis(streamName) {
 	const kinesisInfra = new AWS.Kinesis(
 		Object.assign({}, kinesisStreamOptions, {
 			accessKeyId,
-			secretAccessKey
+			secretAccessKey,
+			signatureCache: false
 		})
 	);
 
@@ -37,6 +38,7 @@ function Kinesis(streamName) {
 		Object.assign({}, kinesisStreamOptions, {
 			accessKeyId: AWS_ACCESS_KEY_ID,
 			secretAccessKey: AWS_SECRET_ACCESS_KEY
+			signatureCache: false
 		})
 	);
 
