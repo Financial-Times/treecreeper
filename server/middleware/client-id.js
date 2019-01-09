01f2580ec73317fe04858711054910011eff2531
Courtney Osborn <courtney.lee.osborn@gmail.com>
2019-01-09 15:11:30 +0000

    Updates to set undefined to null and adds validate header funtion  🐿 v2.11.0

--- server/middleware/client-id.js
+++ server/middleware/client-id.js
@@ -6,34 +6,36 @@ const CLIENT_ID_RX = /^[a-z\d][a-z\d\-\.]*[a-z\d]$/;
 const CLIENT_USER_ID_RX = /^[a-z\d][a-z\d\-\.]*[a-z\d]$/;
 
 module.exports = (req, res, next) => {
+
 	if (!req.get('client-id') && !req.get('client-user-id')) {
 		throw httpErrors(400, 'A client-id or client-user-id header is required');
 	}
 
-	if (req.get('client-id')) {
-		res.locals.clientId = req.get('client-id');
-		setContext('clientId', res.locals.clientId);
+	res.locals.clientId = req.get('client-id') || null;
+	res.locals.clientUserId = req.get('client-user-id') || null;
 
-		if (!CLIENT_ID_RX.test(res.locals.clientId)) {
-			throw httpErrors(
-				400,
-				stripIndents`Invalid client id \`${res.locals.clientId}\`.
-			Must be a string containing only a-z, 0-9, . and -, not beginning or ending with -.`
-			);
-		}
+	if (res.locals.clientId) {
+		const errorMessage = stripIndents`Invalid client id \`${res.locals.clientId}\`.
+			Must be a string containing only a-z, 0-9, . and -, not beginning or ending with -.`;
+
+		setContext('clientId', res.locals.clientId);
+		validateHeader(res.locals.clientId, errorMessage, CLIENT_ID_RX);
+
 	}
 
-	if (req.get('client-user-id')) {
-		res.locals.clientUserId = req.get('client-user-id');
-		setContext('clientUserId', res.locals.clientUserId);
+	if (res.locals.clientUserId) {
+		const errorMessage = stripIndents`Invalid client user id \`${res.locals.clientUserId}\`.
+			It does not appear to be an LDAP user, expecting firstname.surname`;
 
-		if (!CLIENT_USER_ID_RX.test(res.locals.clientUserId)) {
-			throw httpErrors(
-				400,
-				stripIndents`Invalid client user id \`${res.locals.clientUserId}\`.
-			It does not appear to be an LDAP user, expecting firstname.surname`
-			);
-		}
+		setContext('clientUserId', res.locals.clientUserId);
+		validateHeader(res.locals.clientUserId, errorMessage, CLIENT_USER_ID_RX);
+
 	}
 	next();
 };
+
+function validateHeader (header, errorMessage, expectedValueFormat) {
+	if (!expectedValueFormat.test(header)) {
+		throw httpErrors(400, errorMessage);
+	}
+}
