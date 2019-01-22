8a544cd1048ff65e0e7042a531277fb4f5ccc23f
Courtney Osborn <courtney.lee.osborn@gmail.com>
2019-01-22 08:50:48 +0000

    Changes augmentSchema to makeAugmentedSchema  🐿 v2.12.0

--- secret-squirrel.js
+++ secret-squirrel.js
@@ -1,25 +1,23 @@
 module.exports = {
 	files: {
 		allow: [
-			'.prettierrc',
 			'doc/model.cql',
 			'doc/model.grass',
-			'.prettierignore',
 			'scripts/neo4j-plugins',
 			'scripts/neo4j-wait-for-start',
-			'test/mocha.opts',
+			'test/mocha.opts'
 		],
-		allowOverrides: [],
+		allowOverrides: []
 	},
 	strings: {
 		deny: [],
 		denyOverrides: [
-			'd5deb97d-5fa2-45f2-99fa-cd155328320d', // uuid for biz-ops-api dashboard url
-			'bf2e19d8-7601-11e8-864d-61e3701df193', // README.md:4
+			'dba88780-1a7d-11e9-9f6b-e867440c3985', // README.md:4
 			'32e6ad1a-210f-11e8-89cc-978cb917c4e7', // doc/MODEL.md:5
 			'396e2a98-1d16-11e8-87fd-7f7eb4f8a221', // doc/MODEL.md:11
 			'33cba7fc-1d19-11e8-8417-7c85b306fa17', // doc/MODEL.md:21
-			'tech@lt\\.com', // scripts/load-testing/lib/generate/team.js:27
-		],
-	},
+			'd5deb97d-5fa2-45f2-99fa-cd155328320d', // doc/db-upgrade.md:67
+			'tech@lt\\.com' // scripts/load-testing/lib/generate/team.js:29
+		]
+	}
 };
