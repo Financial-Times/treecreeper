adb1f717ae3ba6e4c262c8d6c4019bf51aa0a209
Kat Downes <kathryn.downes@ft.com>
2019-01-24 08:53:47 +0000

    Documentation for changing database plan (#300)
    * Documentation for changing database plan
    
     🐿 v2.12.0
    
    * no need to run tests and adds link to vault folder
     🐿 v2.12.0
    
    * fix typo
    
    * Make instructions to modify plan more clear
     🐿 v2.12.0

--- secret-squirrel.js
+++ secret-squirrel.js
@@ -15,7 +15,7 @@ module.exports = {
 		deny: [],
 		denyOverrides: [
 			'd5deb97d-5fa2-45f2-99fa-cd155328320d', // uuid for biz-ops-api dashboard url
-			'bf2e19d8-7601-11e8-864d-61e3701df193', // README.md:4
+			'dba88780-1a7d-11e9-9f6b-e867440c3985', // README.md:4
 			'32e6ad1a-210f-11e8-89cc-978cb917c4e7', // doc/MODEL.md:5
 			'396e2a98-1d16-11e8-87fd-7f7eb4f8a221', // doc/MODEL.md:11
 			'33cba7fc-1d19-11e8-8417-7c85b306fa17', // doc/MODEL.md:21
