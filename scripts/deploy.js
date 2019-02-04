b920367b92fc7892633ff2039417d77cac0be458
Rhys Evans <rhys.evans@ft.com>
2019-02-04 17:13:31 +0000

    push schema file to s3 on deploy

---
+++ scripts/deploy.js
@@ -0,0 +1,22 @@
+const semver = require('semver');
+const sendToS3 = require('../lib/send-to-s3');
+const rawData = require('../lib/raw-data');
+
+const content = JSON.stringify(
+	{
+		schema: {
+			types: rawData.getTypes(),
+			stringPatterns: rawData.getStringPatterns(),
+			enums: rawData.getEnums(),
+		},
+		version: process.env.CIRCLECI_SHA1,
+	},
+	null,
+	2,
+);
+
+sendToS3(content, {
+	environment: 'latest',
+	majorVersion: semver.major(process.env.CIRCLE_TAG),
+	isPrerelease: !!semver.prerelease(process.env.CIRCLE_TAG),
+});
