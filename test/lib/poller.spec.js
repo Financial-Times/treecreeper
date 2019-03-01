cbdac366ac69f5110ad0372c158faa79114bfc2c
Rhys Evans <rhys.evans@ft.com>
2019-03-01 11:52:51 +0000

    all poller tests complete

--- test/lib/poller.spec.js
+++ test/lib/poller.spec.js
@@ -1,54 +1,95 @@
-const fetch = require('node-fetch');
-
-jest.mock('../../package.json', () => ({ version: '1.2.3' }));
-jest.mock('../../lib/raw-data', () => ({
-	set: jest.fn(),
-	getVersion: () => 1,
-}));
-
-const poller = require('../../lib/poller');
-const rawData = require('../../lib/raw-data');
-// need to mock package.json
-
+/* eslint-disable global-require */
+jest.useFakeTimers();
 describe('poller', () => {
+	let poller;
+	let fetch;
+	let rawData;
 	afterEach(() => {
 		poller.stop();
-		fetch.reset();
-		rawData.set.mockClear();
+		jest.resetModules();
+		jest.clearAllTimers();
 	});
 
 	describe('polling', () => {
-		it('fetches url based on baseUrl and package.json', () => {
+		it('fetches url based on baseUrl and package.json', async () => {
+			fetch = require('node-fetch');
+			jest.doMock('../../package.json', () => ({ version: '1.2.3' }), {
+				virtual: true,
+			});
+			poller = require('../../lib/poller'); // eslint-disable-line global-require
 			fetch.mock('https://base.url/v1.json', { result: true });
-			poller.start('https://base.url');
+			await poller.start('https://base.url');
 			expect(fetch.done()).toEqual(true);
 		});
-		it.skip('adds prerelease to url if prerelease version of library', () => {
-			jest.doMock('../../package.json', () => ({
-				version: '1.2.3-beta.1',
-			}));
-			const prereleasePoller = require('../../lib/poller'); // eslint-disable-line global-require
+
+		it('adds prerelease to url if prerelease version of library', async () => {
+			fetch = require('node-fetch');
+			jest.doMock(
+				'../../package.json',
+				() => ({
+					version: '1.2.3-beta.1',
+				}),
+				{ virtual: true },
+			);
+			poller = require('../../lib/poller'); // eslint-disable-line global-require
 			fetch.mock('https://base.url/v1-prerelease.json', { result: true });
-			prereleasePoller.start('https://base.url');
+			await poller.start('https://base.url');
 			expect(fetch.done()).toEqual(true);
 		});
-		it.skip('fetches url on interval', () => {});
+
+		it('fetches url on interval', async () => {
+			fetch = require('node-fetch');
+			jest.doMock('../../package.json', () => ({ version: '1.2.3' }), {
+				virtual: true,
+			});
+			poller = require('../../lib/poller'); // eslint-disable-line global-require
+			fetch.mock('https://base.url/v1.json', { result: true });
+			await poller.start('https://base.url');
+
+			expect(setInterval).toHaveBeenCalledTimes(1);
+			expect(setInterval).toHaveBeenLastCalledWith(
+				expect.any(Function),
+				20000,
+				'https://base.url/v1.json',
+			);
+			jest.advanceTimersByTime(20000);
+			expect(fetch.calls().length).toEqual(2);
+		});
 	});
 	describe('handling updates', () => {
 		it('no change if existing schema has same version as new one', async () => {
+			jest.doMock('../../lib/raw-data', () => ({
+				set: jest.fn(),
+				getVersion: () => 1,
+			}));
+			rawData = require('../../lib/raw-data');
+			fetch = require('node-fetch');
+			jest.doMock('../../package.json', () => ({ version: '1.2.3' }), {
+				virtual: true,
+			});
+			poller = require('../../lib/poller'); // eslint-disable-line global-require
 			const listener = jest.fn();
 			fetch.mock('https://base.url/v1.json', { version: 1 });
+
 			poller.on('change', listener);
-			poller.start('https://base.url');
-			await new Promise(res => setTimeout(res, 0));
+			await poller.start('https://base.url');
 			expect(listener).not.toHaveBeenCalled();
 		});
 		it('overwrites existing schema if has different version', async () => {
+			jest.doMock('../../lib/raw-data', () => ({
+				set: jest.fn(),
+				getVersion: () => 1,
+			}));
+			rawData = require('../../lib/raw-data');
+			fetch = require('node-fetch');
+			jest.doMock('../../package.json', () => ({ version: '1.2.3' }), {
+				virtual: true,
+			});
+			poller = require('../../lib/poller'); // eslint-disable-line global-require
 			const listener = jest.fn();
 			fetch.mock('https://base.url/v1.json', { version: 2 });
 			poller.on('change', listener);
-			poller.start('https://base.url');
-			await new Promise(res => setTimeout(res, 0));
+			await poller.start('https://base.url');
 			expect(listener).toHaveBeenCalled();
 			expect(rawData.set).toHaveBeenCalledWith({ version: 2 });
 		});
