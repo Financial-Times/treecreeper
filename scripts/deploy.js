6d869a96d34f2a315977bd0c1d3d95cffb53d19e
Kat Downes <kathryn.downes@ft.com>
2019-03-13 11:45:24 +0000

    change to Object.assign

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,13 +1,8 @@
 const sendSchemaToS3 = require('../lib/send-schema-to-s3');
 const rawData = require('../lib/raw-data');
 
-const schemaObject = {
-	schema: {
-		types: rawData.getTypes(),
-		stringPatterns: rawData.getStringPatterns(),
-		enums: rawData.getEnums(),
-	},
+const schemaObject = Object.assign({}, rawData.getAll(), {
 	version: process.env.CIRCLE_TAG,
-};
+});
 
 sendSchemaToS3('latest', schemaObject);
