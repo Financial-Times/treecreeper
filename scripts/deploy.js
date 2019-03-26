c394599023109baf3cb07e68b5319ea9aeb1a0fe
Rhys Evans <rhys.evans@ft.com>
2019-03-26 12:16:24 +0000

    fixed false deploy

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,4 +5,11 @@ const schemaObject = Object.assign({}, new RawData().getAll(), {
 	version: process.env.CIRCLE_TAG,
 });
 
-sendSchemaToS3('latest', schemaObject);
+sendSchemaToS3('latest', schemaObject).then(
+	() => console.log('successfully deployed'),
+	err => {
+		console.error('Failed to deploy');
+		console.error(err);
+		process.exit(2); // eslint-disable-line unicorn/no-process-exit
+	},
+);
