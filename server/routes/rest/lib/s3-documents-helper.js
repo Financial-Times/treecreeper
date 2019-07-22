226e891055c2d396ac042f70383734f3bed3f046
Jennifer.Bacon <jennifer.bacon@ft.com>
2019-07-22 12:12:57 +0100

    extracted uploadToS3 into seperate function

--- server/routes/rest/lib/s3-documents-helper.js
+++ server/routes/rest/lib/s3-documents-helper.js
@@ -11,6 +11,15 @@ const s3BucketReal = () => {
 
 const s3BucketName = 'biz-ops-documents';
 
+const uploadToS3 = async (s3, params, requestType) => {
+	try {
+		const res = await s3.upload(params).promise();
+		logger.info(res, `${requestType}: S3 Upload successful`);
+	} catch (err) {
+		logger.info(err, `${requestType}: S3 Upload failed`);
+	}
+};
+
 const writeFileToS3 = async (
 	nodeType,
 	code,
@@ -23,12 +32,7 @@ const writeFileToS3 = async (
 		Key: `${nodeType}/${code}`,
 		Body: JSON.stringify(body),
 	};
-	try {
-		const res = await s3.upload(params).promise();
-		logger.info(res, 'POST: S3 Upload successful');
-	} catch (err) {
-		logger.info(err, 'POST: S3 Upload failed');
-	}
+	uploadToS3(s3, params, 'POST');
 };
 
 const patchS3file = async (
@@ -47,43 +51,20 @@ const patchS3file = async (
 		const existingBody = JSON.parse(existingNode.Body);
 		if (diff(existingBody, body)) {
 			const newBody = Object.assign(existingBody, body);
-			try {
-				const res = await s3
-					.upload(
-						Object.assign(
-							{ Body: JSON.stringify(newBody) },
-							params,
-						),
-					)
-					.promise();
-				logger.info(
-					res,
-					'PATCH: S3 Upload successful - file has been updated',
-				);
-			} catch (uploadErr) {
-				logger.info(
-					uploadErr,
-					'PATCH: S3 Upload failed - could not update file',
-				);
-			}
+			uploadToS3(
+				s3,
+				Object.assign({ Body: JSON.stringify(newBody) }, params),
+				'PATCH',
+			);
 		} else {
 			logger.info('PATCH: No S3 Upload as file is unchanged');
 		}
 	} catch (err) {
-		try {
-			const res = await s3
-				.upload(Object.assign({ Body: JSON.stringify(body) }, params))
-				.promise();
-			logger.info(
-				res,
-				'PATCH: S3 Upload successful - file has been created',
-			);
-		} catch (uploadErr) {
-			logger.info(
-				uploadErr,
-				'PATCH: S3 Upload failed - could not create file',
-			);
-		}
+		uploadToS3(
+			s3,
+			Object.assign({ Body: JSON.stringify(body) }, params),
+			'PATCH',
+		);
 	}
 };
 
