ac5b83943eaa0eaae72114140a9acb4939df7d27
Rhys Evans <rhys.evans@ft.com>
2019-08-23 13:25:50 +0100

    sketch out how schema validaor will work

---
+++ packages/schema-validator/index.js
@@ -0,0 +1,6 @@
+
+// TODO
+// consumer shodul allow initialisation by being given path to directory or schema content
+// path can be an env var
+// so to run validator, set env var, then call as cli
+// write as assertions - no jest??
