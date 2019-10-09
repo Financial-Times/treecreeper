8d9ff6a3868a635802f1ba5eef0d24914f51f613
Rhys Evans <rhys.evans@ft.com>
2019-10-09 16:30:47 +0100

    playing with POST

--- packages/api-core/lib/neo4j-type-conversion.js
+++ packages/api-core/lib/neo4j-type-conversion.js
@@ -3,11 +3,11 @@ const {
 	isDate,
 	isTime,
 } = require('neo4j-driver/lib/v1/temporal-types');
-// const neo4jTemporalTypes = require('neo4j-driver/lib/v1/temporal-types');
-// const { getType } = require('../../../packages/schema-sdk');
-// const { diffProperties } = require('./diff-helpers');
+const neo4jTemporalTypes = require('neo4j-driver/lib/v1/temporal-types');
+const { getType } = require('../../../packages/schema-sdk');
+const { diffProperties } = require('./diff-helpers');
 
-// const entriesToObject = (map, [key, val]) => Object.assign(map, { [key]: val });
+const entriesToObject = (map, [key, val]) => Object.assign(map, { [key]: val });
 
 const isTemporalType = val => isDateTime(val) || isDate(val) || isTime(val);
 const convertNeo4jToJson = obj => {
@@ -19,41 +19,41 @@ const convertNeo4jToJson = obj => {
 	return obj;
 };
 
-// const isTemporalTypeName = type => ['Date', 'DateTime', 'Time'].includes(type);
+const isTemporalTypeName = type => ['Date', 'DateTime', 'Time'].includes(type);
 
-// const convertTemporalTypes = nodeType => {
-// 	const { properties } = getType(nodeType);
-// 	return ([propName, val]) => {
-// 		const { type } = properties[propName];
-// 		if (isTemporalTypeName(type)) {
-// 			val = neo4jTemporalTypes[type].fromStandardDate(new Date(val));
-// 		}
-// 		return [propName, val];
-// 	};
-// };
+const convertTemporalTypes = nodeType => {
+	const { properties } = getType(nodeType);
+	return ([propName, val]) => {
+		const { type } = properties[propName];
+		if (isTemporalTypeName(type)) {
+			val = neo4jTemporalTypes[type].fromStandardDate(new Date(val));
+		}
+		return [propName, val];
+	};
+};
 
-// const isNullValue = val => val === null || val === '';
+const isNullValue = val => val === null || val === '';
 
-// const convertNullValues = ([propName, val]) => [
-// 	propName,
-// 	isNullValue(val) ? null : val,
-// ];
+const convertNullValues = ([propName, val]) => [
+	propName,
+	isNullValue(val) ? null : val,
+];
 
-// const constructNeo4jProperties = ({
-// 	nodeType,
-// 	newContent = {},
-// 	code,
-// 	initialContent,
-// }) => {
-// 	newContent.code = code || newContent.code;
+const constructNeo4jProperties = ({
+	nodeType,
+	newContent = {},
+	code,
+	initialContent,
+}) => {
+	newContent.code = code || newContent.code;
 
-// 	return diffProperties({ nodeType, newContent, initialContent }, false)
-// 		.map(convertTemporalTypes(nodeType))
-// 		.map(convertNullValues)
-// 		.reduce(entriesToObject, {});
-// };
+	return diffProperties({ nodeType, newContent, initialContent }, false)
+		.map(convertTemporalTypes(nodeType))
+		.map(convertNullValues)
+		.reduce(entriesToObject, {});
+};
 
 module.exports = {
 	convertNeo4jToJson,
-	// constructNeo4jProperties,
+	constructNeo4jProperties,
 };
--- packages/api-rest-post-handler/index.js
+++ packages/api-rest-post-handler/index.js
@@ -1,7 +1,48 @@
-const { validateInput } = require('../api-core/lib/validation');
+const { validateInput } = require('../../packages/api-core/lib/validation');
+const { metaPropertiesForCreate } = require('../../packages/api-core/lib/metadata-helpers');
+const { getType } = require('../../packages/schema-sdk');
+const { getAddedRelationships } = require('../../packages/api-core/lib/diff-helpers');
+const { constructNeo4jProperties } = require('../../packages/api-core/lib/neo4j-type-conversion');
 
 const postHandler = ({ documentStore } = {}) => async input => {
 	const { type, code, body, metadata, query } = validateInput(input);
+
+	const { createPermissions, pluralName } = getType(nodeType);
+	if (createPermissions && !createPermissions.includes(metadata.clientId)) {
+		throw httpError(
+			400,
+			`${pluralName} can only be created by ${createPermissions.join(
+				', ',
+			)}`,
+		);
+	}
+
+	const neo4jResult = await getNeo4jRecord(type, code);
+
+	if (neo4jResult.hasRecords()) {
+		throw httpErrors(409, `${type} ${code} already exists`);
+	}
+
+	const queryParts = [stripIndents`
+			CREATE (node:${type} $properties)
+				SET ${metaPropertiesForCreate('node')}
+			WITH node`]
+
+	const properties = constructNeo4jProperties({
+		type,
+		newContent: body,
+		code,
+	})
+
+	const relationshipsToCreate = getAddedRelationships({
+		type,
+		newContent: bodyNoDocs,
+	}),
+
+
+
 };
 
 module.exports = { postHandler };
+
+
