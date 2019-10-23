29b7bacb1e503a595d3196719188f3ee99692446
Rhys Evans <rhys.evans@ft.com>
2019-10-23 11:07:47 +0100

    reinstate waiting for schema in db-manager

--- packages/api-db-manager/index.js
+++ packages/api-db-manager/index.js
@@ -7,6 +7,7 @@ const dbConnection = require('./db-connection');
 const exclusion = (arr1, arr2) => arr1.filter(val => !arr2.includes(val));
 
 const initConstraints = async () => {
+	await schema.ready();
 	const executeQuery = dbConnection.executeQueryWithSharedSession();
 
 	try {
