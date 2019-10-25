bf6680430a5d25f75c59975bf176c0da60ca6d61
Asuka Ochi <asuka.ochi@ft.com>
2019-10-25 11:42:29 +0100

    Change originalBody to body in separateDoc function

--- packages/api-rest-handlers/lib/separate-documents-from-body.js
+++ packages/api-rest-handlers/lib/separate-documents-from-body.js
@@ -1,21 +1,21 @@
 const _isEmpty = require('lodash.isempty');
 const { getType } = require('../../schema-sdk');
 
-const separateDocsFromBody = (nodeType, originalBody = {}) => {
+const separateDocsFromBody = (nodeType, body = {}) => {
 	const { properties } = getType(nodeType);
 	const bodyDocuments = {};
 	const bodyNoDocs = {};
 
-	Object.entries(originalBody).forEach(([key, value]) => {
+	Object.entries(body).forEach(([key, value]) => {
 		if (properties[key].type === 'Document') {
 			// We should check the value is empty only when type is Document
 			// because other type will be passed as boolean, number, etc...
 			// then _isEmpty() will returns true
 			if (!_isEmpty(value)) {
-				bodyDocuments[key] = originalBody[key];
+				bodyDocuments[key] = body[key];
 			}
 		} else {
-			bodyNoDocs[key] = originalBody[key];
+			bodyNoDocs[key] = body[key];
 		}
 	});
 
