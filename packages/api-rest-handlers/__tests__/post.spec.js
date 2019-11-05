c590e7e7ab7ef6b4e73506bae2b72cabf98523a6
Asuka Ochi <asuka.ochi@ft.com>
2019-11-05 12:33:33 +0000

    Modify POST tests

--- packages/api-rest-handlers/__tests__/post.spec.js
+++ packages/api-rest-handlers/__tests__/post.spec.js
@@ -301,18 +301,13 @@ describe('rest POST', () => {
 			);
 
 			expect(status).toBe(200);
-			[...body.children, ...body.parents].forEach(relationship =>
-				expect(relationship).toHaveProperty(
-					'code',
-					'_updatedByClient',
-					'_updatedByRequest',
-					'_updatedTimestamp',
-					'_updatedByUser',
-					'_createdByClient',
-					'_createdByRequest',
-					'_createdTimestamp',
-					'_createdByUser',
-				),
+
+			body.children.forEach(relationship =>
+				expect(relationship).toMatchObject({ code: childCode, ...meta.create }),
+			);
+
+			body.parents.forEach(relationship =>
+				expect(relationship).toMatchObject({ code: parentCode, ...meta.create }),
 			);
 		});
 	});
