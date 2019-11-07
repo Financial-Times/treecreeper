242d694d24e98d31c0226a09deb108928c9e8e46
Rhys Evans <rhys.evans@ft.com>
2019-11-07 11:30:09 +0000

    remove stray log

--- test-helpers/extend-jest.js
+++ test-helpers/extend-jest.js
@@ -1,7 +1,6 @@
 /* global expect */
 expect.extend({
 	httpError(result, expected) {
-		console.log({ result, expected, status: result.status });
 		expect(result.status).toEqual(expected.status);
 		expect(result).toMatchObject({
 			message: expect.stringMatching(expected.message),
