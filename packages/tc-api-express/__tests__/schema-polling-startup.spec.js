252445254dc145d19ab7d2ccc6f7880c08650eac
Rhys Evans <rhys.evans@ft.com>
2019-11-25 12:36:33 +0000

    Reimplement schema polling tests in  package
    As getApp now takes care of initialising the schema and database,m
    it's appropriate that the tests live in tc-api-express

---
+++ packages/tc-api-express/__tests__/schema-polling-startup.spec.js
@@ -0,0 +1,32 @@
+const fetch = require('node-fetch');
+const { getSchemaFilename } = require('@financial-times/tc-schema-file-name');
+const { getApp } = require('..');
+
+describe('schema polling startup', () => {
+	beforeAll(() => {
+		fetch.config.fallbackToNetwork = false;
+	});
+	afterAll(() => {
+		fetch.config.fallbackToNetwork = 'always';
+	});
+
+	it('does not resolve promise for app until schema is hydrated when polling', async () => {
+		delete process.env.TREECREEPER_SCHEMA_DIRECTORY;
+		process.env.TEST_STARTUP = true;
+		process.env.TREECREEPER_SCHEMA_URL = 'http://example.com';
+		fetch.mock('*', {});
+		let initialised = false;
+		const promiseOfApp = getApp({ schemaOptions: { updateMode: 'poll' } });
+		expect(initialised).toBe(false);
+		promiseOfApp.then(() => {
+			initialised = true;
+		});
+		await fetch.flush(true);
+		expect(fetch.lastUrl()).toEqual(
+			`http://example.com/${getSchemaFilename()}`,
+		);
+		await new Promise(res => setTimeout(res));
+		expect(initialised).toBe(true);
+		fetch.reset();
+	});
+});
---
+++ packages/tc-api-express/__tests__/schema-polling-updates.spec.js
@@ -0,0 +1,228 @@
+const fetch = require('node-fetch');
+const schemaPublisher = require('@financial-times/tc-schema-publisher');
+const request = require('supertest');
+const { getSchemaFilename } = require('@financial-times/tc-schema-file-name');
+const { getApp } = require('..');
+
+jest.useFakeTimers();
+jest.mock('@financial-times/tc-schema-publisher', () => ({
+	sendSchemaToS3: jest.fn(),
+}));
+
+// Skipping for now as the consant refactors play havoc with jest's mocking
+describe('schema polling updates', () => {
+	describe('api updates', () => {
+		let app;
+		beforeAll(async () => {
+			process.env.NODE_ENV = 'production';
+			delete process.env.TREECREEPER_SCHEMA_DIRECTORY;
+			process.env.TREECREEPER_SCHEMA_URL = 'http://example.com';
+			schemaPublisher.sendSchemaToS3.mockResolvedValue(true);
+			fetch.config.fallbackToNetwork = false;
+			fetch
+				.getOnce(
+					`${
+						process.env.TREECREEPER_SCHEMA_URL
+					}/${getSchemaFilename()}`,
+					{
+						version: 'not-null',
+						schema: {
+							types: [
+								{
+									name: 'OldTestType',
+									description: 'A test type.',
+									properties: {
+										code: {
+											type: 'Code',
+											description: 'The code.',
+											canIdentify: true,
+										},
+										testProp: {
+											type: 'Paragraph',
+											description: 'A test property.',
+										},
+									},
+								},
+							],
+							enums: {},
+							stringPatterns: {},
+						},
+					},
+				)
+				.getOnce(
+					`${
+						process.env.TREECREEPER_SCHEMA_URL
+					}/${getSchemaFilename()}`,
+					{
+						version: 'new-test',
+						schema: {
+							types: [
+								{
+									name: 'TestType',
+									description: 'A test type.',
+									properties: {
+										code: {
+											type: 'Code',
+											description: 'The code.',
+											canIdentify: true,
+										},
+										testProp: {
+											type: 'Paragraph',
+											description: 'A test property.',
+										},
+									},
+								},
+							],
+							enums: {},
+							stringPatterns: {},
+						},
+					},
+					{ overwriteRoutes: false },
+				)
+				.catch(200);
+			app = await getApp({
+				republishSchemaPrefix: 'custom',
+				republishSchema: true,
+				schemaOptions: { updateMode: 'poll', ttl: 100 },
+			});
+			await fetch.flush(true);
+			jest.advanceTimersByTime(101);
+			await fetch.flush(true);
+		});
+		afterAll(() => {
+			process.env.NODE_ENV = 'test';
+			fetch.config.fallbackToNetwork = 'always';
+			fetch.reset();
+			jest.resetModules();
+		});
+
+		describe('success', () => {
+			it('constructs new graphql api', async () => {
+				return request(app, { useCached: false })
+					.post('/graphql')
+					.send({
+						query: `
+						{
+							TestType {
+								testProp
+							}
+						}
+					`,
+					})
+					.set('client-id', 'polling-client')
+					.expect(200);
+			});
+
+			it('updates validation rules', async () => {
+				return request(app, { useCached: false })
+					.head(`/rest/MainType/main-code-${Date.now()}`)
+					.set('client-id', 'polling-client')
+					.expect(400);
+			});
+
+			it('writes the latest schema to the S3 api endpoint', () => {
+				expect(schemaPublisher.sendSchemaToS3).toHaveBeenCalledWith(
+					'custom',
+				);
+			});
+
+			it('surfaces good state via a method', async () => {
+				expect(app.treecreeper.isSchemaUpdating()).toEqual(true);
+			});
+		});
+		describe('failure', () => {
+			beforeAll(async () => {
+				process.env.NODE_ENV = 'production';
+				schemaPublisher.sendSchemaToS3.mockClear();
+
+				fetch
+					.getOnce(
+						`${
+							process.env.TREECREEPER_SCHEMA_URL
+						}/${getSchemaFilename()}`,
+						{
+							version: 'new-test2',
+							schema: {
+								types: [
+									{
+										name: 'InvalidType',
+										description: 'An invalid type.',
+										properties: {
+											// have added some bits that'll generate invalid graphql schema
+											'code\nmultiline': {
+												type: 'Code',
+												description: 'The code.',
+											},
+											testProp: {
+												type: 'Paragraph',
+												description: 'A test property.',
+											},
+										},
+									},
+								],
+								enums: {},
+								stringPatterns: {},
+							},
+						},
+						{ overwriteRoutes: false },
+					)
+					.catch(200);
+				jest.advanceTimersByTime(101);
+				await fetch.flush(true);
+			});
+			afterAll(() => {
+				process.env.NODE_ENV = 'test';
+			});
+
+			it('graphql endpoint still runs on old schema version', async () => {
+				await request(app, { useCached: false })
+					.post('/graphql')
+					.send({
+						query: `
+							{
+								InvalidType {
+									testProp
+								}
+							}
+						`,
+					})
+					.set('client-id', 'polling-client')
+					.expect(400);
+				await request(app, { useCached: false })
+					.post('/graphql')
+					.send({
+						query: `
+							{
+								TestType {
+									testProp
+								}
+							}
+						`,
+					})
+					.set('client-id', 'polling-client')
+					.expect(200);
+			});
+
+			it('does not send the schema to s3', () => {
+				expect(schemaPublisher.sendSchemaToS3).not.toHaveBeenCalled();
+			});
+
+			it('surfaces failed state via a method', async () => {
+				expect(app.treecreeper.isSchemaUpdating()).toEqual(false);
+			});
+		});
+	});
+
+	// Not testing this as directly as I'd like as it's tricky
+	it.skip('reinitialises database contraints', async () => {
+		const onChange = jest.fn();
+		jest.doMock('@financial-times/tc-schema-sdk', () => ({
+			init: () => null,
+			onChange,
+			ready: () => Promise.resolve(),
+		}));
+		// const { initConstraints } = require('../server/init-db');
+		// expect(onChange).toHaveBeenCalledWith(initConstraints);
+		jest.dontMock('@financial-times/tc-schema-sdk');
+	});
+});
