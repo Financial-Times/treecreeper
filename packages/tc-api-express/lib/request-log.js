016236111191bae223cf8d7a31b1d6309cf8b8bb
Rhys Evans <rhys.evans@ft.com>
2019-11-26 20:32:34 +0000

    request log for graphql

---
+++ packages/tc-api-express/lib/request-log.js
@@ -0,0 +1,15 @@
+	logger,
+	setContext,
+} = require('@financial-times/tc-api-express-logger');
+const requestLog = (endpointName, method, req) => {
+	setContext({
+		endpointName,
+		method,
+		params: req.params,
+		query: req.query,
+		bodyKeys: Object.keys(req.body || {}),
+	});
+	logger.info(`[APP] ${endpointName} ${method}`);
+};
+
+module.exports = {requestLog}
