41e41dcbb6fab31d0566a4c76343ac95873fd6d1
Rhys Evans <rhys.evans@ft.com>
2019-11-27 08:55:35 +0000

    corrected db timeout method name

--- packages/tc-api-db-manager/db-connection.js
+++ packages/tc-api-db-manager/db-connection.js
@@ -64,7 +64,7 @@ driver.session = (...sessionArgs) => {
 
 module.exports = {
 	driver,
-	setDbQueryTimeout: timeout => {
+	setTimeout: timeout => {
 		TIMEOUT = timeout;
 	},
 	executeQuery: (query, parameters) =>
