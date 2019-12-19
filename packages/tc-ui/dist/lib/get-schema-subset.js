1481e44451736324fe52288efe86eccde13c25da
Rhys Evans <rhys.evans@ft.com>
2019-12-19 14:15:54 +0000

    transpile tc-ui before publishing

---
+++ packages/tc-ui/dist/lib/get-schema-subset.js
@@ -0,0 +1,44 @@
+"use strict";
+
+const {
+  getType
+} = require('@financial-times/tc-schema-sdk');
+
+const getTypeOptions = {
+  groupProperties: true,
+  includeMetaFields: false
+};
+
+const getSchemaSubset = (event, type, isCreate = false) => {
+  const properties = event.query && event.query.properties;
+
+  if (!properties) {
+    return {
+      schema: getType(type, { ...getTypeOptions,
+        useMinimumViableRecord: isCreate
+      }),
+      isSubset: false
+    };
+  }
+
+  const title = event.query && event.query.title;
+  const fullSchema = getType(type, getTypeOptions);
+  const propertyKeys = properties.split(',');
+  return {
+    schema: { ...fullSchema,
+      fieldsets: {
+        [title]: {
+          heading: title || 'Properties',
+          properties: Object.entries(fullSchema.properties).filter(([propName]) => propertyKeys.includes(propName)).reduce((fieldset, [propName, propDef]) => ({ ...fieldset,
+            [propName]: propDef
+          }), {})
+        }
+      }
+    },
+    isSubset: true
+  };
+};
+
+module.exports = {
+  getSchemaSubset
+};
