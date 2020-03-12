a5772aa12928ff99b9164c6400100f9b1b65e857
Rhys Evans <rhys.evans@ft.com>
2020-03-12 20:14:05 +0000

    cypress tests for integer field errors

--- packages/tc-ui/src/primitives/number/__tests__/e2e-record-number.cyp.js
+++ packages/tc-ui/src/primitives/number/__tests__/e2e-record-number.cyp.js
@@ -7,6 +7,7 @@ const {
 	populateMinimumViableFields,
 	save,
 	resetDb,
+	visitEditPage
 } = require('../../../test-helpers/cypress');
 
 describe('End-to-end - record Number type', () => {
@@ -16,28 +17,55 @@ describe('End-to-end - record Number type', () => {
 		});
 	});
 
-	it('can record an integer', () => {
-		cy.get('input[name=someInteger]').type(someInteger);
-		save();
+	describe('int', () => {
 
-		cy.get('#code').should('have.text', code);
-		cy.get('#someString').should('have.text', someString);
-		cy.get('#someInteger').should('have.text', String(someInteger));
-	});
+		it('can record an integer', () => {
+			cy.get('input[name=someInteger]').type(someInteger);
+			save();
 
-	it('can not accept non-integer value', () => {
-		cy.get('input[name=someInteger]').type(someInteger / 7);
-		save();
+			cy.get('#code').should('have.text', code);
+			cy.get('#someString').should('have.text', someString);
+			cy.get('#someInteger').should('have.text', String(someInteger));
+		});
 
-		cy.url().should('contain', '/MainType/create');
-		cy.get('.o-message__content-main').should(
-			'contain',
-			'Oops. Could not create MainType record for e2e-demo',
-		);
-		cy.get('.o-message__content-additional').should(
-			'contain',
-			`Invalid value \`${someInteger /
-				7}\` for property \`someInteger\` on type \`MainType\`: Must be a finite integer`,
-		);
-	});
+		it('rejects floats', () => {
+			cy.get('input[name=someInteger]').type('0.5');
+			save();
+
+			cy.url().should('contain', '/MainType/create');
+			cy.get('.o-message__content-main').should(
+				'contain',
+				'Oops. Could not create MainType record for e2e-demo',
+			);
+			cy.get('.o-message__content-additional').should(
+				'contain',
+				`Invalid value \`0.5\` for property \`someInteger\` on type \`MainType\`: Must be a finite integer`,
+			);
+		});
+
+		it('rejects text', () => {
+			cy.get('input[name=someInteger]').type('haha');
+			save();
+
+			cy.url().should('contain', '/MainType/create');
+			cy.get('.o-message__content-main').should(
+				'contain',
+				'Oops. Could not create MainType record for e2e-demo',
+			);
+			cy.get('.o-message__content-additional').should(
+				'contain',
+				`Invalid value \`haha\` for property \`someInteger\` on type \`MainType\`: Must be a finite integer`,
+			);
+		});
+
+		it('saves and redisplays zero', () => {
+			cy.get('input[name=someInteger]').type('0');
+			save();
+
+			cy.url().should('contain', '/MainType/create');
+			cy.get('#someInteger').should('have.text', '0');
+			visitEditPage()
+			cy.get('input[name=someInteger]').should('have.value', '0');
+		});
+	})
 });
