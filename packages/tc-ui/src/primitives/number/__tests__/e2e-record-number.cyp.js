8a09fccca68f4cf48f345c41c832c857722a701c
Rhys Evans <rhys.evans@ft.com>
2020-03-12 20:58:23 +0000

    implement for floats

--- packages/tc-ui/src/primitives/number/__tests__/e2e-record-number.cyp.js
+++ packages/tc-ui/src/primitives/number/__tests__/e2e-record-number.cyp.js
@@ -68,4 +68,49 @@ describe('End-to-end - record Number type', () => {
 			cy.get('input[name=someInteger]').should('have.value', '0');
 		});
 	})
+
+	describe('float', () => {
+
+		it('can record an integer', () => {
+			cy.get('input[name=someFloat]').type(someInteger);
+			save();
+
+			cy.get('#code').should('have.text', code);
+			cy.get('#someString').should('have.text', someString);
+			cy.get('#someFloat').should('have.text', String(someInteger));
+		});
+
+		it('accepts floats', () => {
+			cy.get('input[name=someFloat]').type('0.5');
+			save();
+
+			cy.url().should('contain', '/MainType/e2e-demo');
+			cy.get('#someFloat').should('have.text', '0.5');
+		});
+
+		it('rejects text', () => {
+			cy.get('input[name=someFloat]').type('haha');
+			save();
+
+			cy.url().should('contain', '/MainType/create');
+			cy.get('.o-message__content-main').should(
+				'contain',
+				'Oops. Could not create MainType record for e2e-demo',
+			);
+			cy.get('.o-message__content-additional').should(
+				'contain',
+				`Invalid value \`haha\` for property \`someFloat\` on type \`MainType\`: Must be a finite floating point number`,
+			);
+		});
+
+		it('saves and redisplays zero', () => {
+			cy.get('input[name=someFloat]').type('0');
+			save();
+
+			cy.url().should('contain', '/MainType/e2e-demo');
+			cy.get('#someFloat').should('have.text', '0');
+			visitEditPage()
+			cy.get('input[name=someFloat]').should('have.value', '0');
+		});
+	})
 });
