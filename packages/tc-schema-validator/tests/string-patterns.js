c078ce56d9fa2c1a6b4f7bff2b0fd06c6dd988ee
Rhys Evans <rhys.evans@ft.com>
2020-04-08 10:59:06 +0100

    Stop validating that string pattern regexes disallow long strings
    It's up to individual regexes to do this, and I don't think the test, as written, was working as intended anyway because most of the patterns did not have length patterns

--- packages/tc-schema-validator/tests/string-patterns.js
+++ packages/tc-schema-validator/tests/string-patterns.js
@@ -4,17 +4,12 @@ const { SDK } = require('@financial-times/tc-schema-sdk');
 const sdk = new SDK();
 sdk.init();
 const stringPatterns = sdk.rawData.getStringPatterns();
-const longString = 'x'.repeat(257);
 
 describe('string patterns', () => {
 	Object.entries(stringPatterns).forEach(([name, pattern]) => {
 		if (typeof pattern === 'string') {
 			it(`${name} evaluates to valid flagless regex`, () => {
 				expect(() => new RegExp(pattern)).not.toThrow();
-			});
-			it(`${name} blocks very long strings`, () => {
-				const regex = new RegExp(pattern);
-				expect(regex.test(longString)).toEqual(false);
 			});
 		} else {
 			it(`${name} evaluates to valid flagged regex`, () => {
