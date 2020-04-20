fb5cf27dfb6d83473e3ef73557d36b3cbe8f770e
katsuraku <kirstenjones@yahoo.co.uk>
2020-04-20 10:44:35 +0100

    Update the form data

--- packages/tc-ui/src/pages/edit/handler.js
+++ packages/tc-ui/src/pages/edit/handler.js
@@ -14,7 +14,7 @@ const getEditHandler = ({
 		const apiClient = getApiClient(event);
 		const { type, code } = event;
 		const isCreate = !code;
-
+console.log('55555 event', JSON.stringify(event, null,2))
 		let formData;
 
 		if (apiError) {
@@ -27,7 +27,7 @@ const getEditHandler = ({
 			// otherwise serve a blank /create form
 			formData = {};
 		}
-
+console.log('66666 formData: ', JSON.stringify(formData, null,4))
 		const templateData = {
 			...getSchemaSubset(event, type, isCreate),
 			data: formData,
@@ -40,7 +40,7 @@ const getEditHandler = ({
 				? `Edit ${type} ${formData.name}`
 				: `New ${type}`,
 		};
-
+console.log('77777 templateData: ', JSON.stringify(templateData, null,2))
 		if (apiError) {
 			templateData.error = apiError;
 		}
@@ -53,6 +53,11 @@ const getEditHandler = ({
 		const { type } = event;
 		let { code } = event;
 		const formData = event.body;
+
+		if (formData.DecomOverride) { // either this or the override component uses the original lifecycle stage key
+			formData.lifecycleStage = 'Decommissioned';
+			// delete formData.DecomOverride;
+		}
 		let method = 'PATCH';
 		// If a code is present in the url/event then PATCH the existing record
 		// otherwise POST the data to create a new item
@@ -61,6 +66,11 @@ const getEditHandler = ({
 			method = 'POST';
 		}
 		try {
+			console.log('09090909 type', type)
+			console.log('09090909 code', code)
+			const fdtr = formDataToRest(type, formData);
+			console.log('0909090', JSON.stringify(fdtr, null, 2))
+			console.log('09090909 method', method)
 			await apiClient.write(
 				type,
 				code,
@@ -74,9 +84,10 @@ const getEditHandler = ({
 				action: method === 'POST' ? 'create' : 'update',
 				message: err.message,
 			};
+			console.log('1212121 error', error)
 			return displayForm(event, error);
 		}
-
+console.log('9999 method', method)
 		return {
 			status: 303,
 			headers: {
@@ -93,6 +104,7 @@ const getEditHandler = ({
 			return displayForm(event);
 		}
 		if (event.method === 'POST') {
+			console.log('88888 POST')
 			return handleForm(event);
 		}
 		throw httpError(405, 'Method Not Allowed');
