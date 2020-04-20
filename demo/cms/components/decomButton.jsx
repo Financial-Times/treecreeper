b2d2aee597bdd5eff99b06d5cc73314b24de620c
katsuraku <kirstenjones@yahoo.co.uk>
2020-04-20 15:15:04 +0100

    Remove stuff for checkbox, go back to button

--- demo/cms/components/decomButton.jsx
+++ demo/cms/components/decomButton.jsx
@@ -1,74 +1,61 @@
 const React = require('react');
+const fetch = require('node-fetch');
+
+const onDecom = async props => {
+	const fetchtodo = `./decommission`;
+	const response = await fetch(fetchtodo);
+	const json = await response.json();
+	console.log('json response: ', json);
+};
 
 const DecomButton = props => {
-	console.log('props in decom button', props);
-	if (
-		props.lockedBy !== 'biz-ops-runbook-md' ||
-		props.lifecycleStage === 'Decommissioned'
-	) {
-		return null;
-	}
+	console.log('props in decom button');
 	return (
 		<div
 			className="decommission-override"
 			id="decommission-override"
 			data-props={JSON.stringify(props)}
 		>
-			<div
-				className="o-forms-field decommission-form"
-				role="group"
-				aria-labelledby="checkbox-group-title"
-				aria-describedby="checkbox-group-info"
-			>
-				<span className="o-forms-input o-forms-input--checkbox">
-					<label>
-						<input
-							type="checkbox"
-							name="DecomOverride"
-							value
-							aria-label="DecomOverride"
-						/>
-						<span
-							className="o-forms-input__label"
-							aria-hidden="true">
-							Mark this system as Decommissioned
-						</span>
-					</label>
+			<div className="inline tooltip-container">
+				Mark this system as Decommissioned
+				<span
+					className="tooltip-target-decomOverride treecreeper-help"
+					id="tooltip-target-decomOverride"
+				>
+					<i
+						aria-label="help for decomOverride"
+						className="o-icons-icon o-icons-icon--info treecreeper-help-icon"
+					/>
 				</span>
 				<div
-					className="inline tooltip-container o-forms-title__main"
-					id="inline-radio-box-group-title"
+					data-o-component="o-tooltip"
+					data-o-tooltip-position="below"
+					data-o-tooltip-target="tooltip-target-decomOverride"
+					data-o-tooltip-show-on-click="true"
+					role="tooltip"
+					className="o-tooltip"
 				>
-
-					<span
-						className="tooltip-target-decomOverride treecreeper-help"
-						id="tooltip-target-decomOverride"
-					>
-						<i
-							aria-label="help for decomOverride"
-							className="o-icons-icon o-icons-icon--info treecreeper-help-icon"
-						/>
-					</span>
-					<div
-						data-o-component="o-tooltip"
-						data-o-tooltip-position="below"
-						data-o-tooltip-target="tooltip-target-decomOverride"
-						data-o-tooltip-show-on-click="true"
-						role="tooltip"
-						className="o-tooltip"
-					>
-						<div className="o-tooltip-content">
-							Check the box and Save to decommission a system
-							locked by RunbookMD.
-						</div>
-						<button
-							className="o-tooltip-close"
-							aria-label="Close tooltip"
-							title="Close tooltip"
-						/>
+					<div className="o-tooltip-content">
+						This is an override for systems locked by Runbook MD,
+						here for when there is no other way to decommission the
+						system.
 					</div>
+					<button
+						className="o-tooltip-close"
+						aria-label="Close tooltip"
+						title="Close tooltip"
+						type="button"
+					/>
 				</div>
 			</div>
+
+			<button
+				type="button"
+				className="o-buttons o-buttons--secondary o-buttons--small decom-button"
+				onClick={() => onDecom(props)}
+			>
+				Decommission
+			</button>
 		</div>
 	);
 };
