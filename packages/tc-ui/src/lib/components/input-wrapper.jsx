a10a78205a8dd1b8b6214e0889c6ed4663c76c81
geoff thorpe <geoff.thorpe@ft.com>
2020-04-29 14:50:55 +0100

    Spelling and linking

--- packages/tc-ui/src/lib/components/input-wrapper.jsx
+++ packages/tc-ui/src/lib/components/input-wrapper.jsx
@@ -6,55 +6,59 @@ const { LinkToRecord } = require('./structure');
 const FieldTitle = ({ label, description, expandableContent, lockedBy }) => {
 	const converter = new showdown.Converter();
 	const descriptionHTML = converter.makeHtml(description);
-	const expandableContentHTML = converter.makeHtml(expandedContent);
+	const expandableContentHTML = converter.makeHtml(expandableContent);
 	return (
 		<span className="o-forms-title">
-		<span
-			className="o-forms-title__main"
-			id="inline-radio-round-group-title"
-		>
-			{label}:
-		</span>
-
-		<span className="o-forms-title__prompt description-text">
 			<span
-				dangerouslySetInnerHTML={{
-					__html:
-						typeof window === 'undefined'
-							? autolinker.link(descriptionHTML)
-							: // eslint-disable-next-line no-undef
-							Autolinker.link(descriptionHTML),
-				}}
-			/>
-			{expandableContent ? (
-				<div
-					data-o-component="o-expander"
-					className="o-expander"
-					data-o-expander-shrink-to="hidden"
-					data-o-expander-collapsed-toggle-text="more info"
-					data-o-expander-expanded-toggle-text="less"
-				>
-					{' '}
-					<button
-						className="o-expander__toggle o--if-js"
-						type="button"
+				className="o-forms-title__main"
+				id="inline-radio-round-group-title"
+			>
+				{label}:
+			</span>
+
+			<span className="o-forms-title__prompt description-text">
+				<span
+					dangerouslySetInnerHTML={{
+						__html:
+							typeof window === 'undefined'
+								? autolinker.link(descriptionHTML)
+								: // eslint-disable-next-line no-undef
+								  Autolinker.link(descriptionHTML),
+					}}
+				/>
+				{expandableContent ? (
+					<div
+						data-o-component="o-expander"
+						className="o-expander"
+						data-o-expander-shrink-to="hidden"
+						data-o-expander-collapsed-toggle-text="more info"
+						data-o-expander-expanded-toggle-text="less"
 					>
-						more info
-					</button>
-					<div className="o-expander__content">
-						{expandableContentHTML}
+						{' '}
+						<button
+							className="o-expander__toggle o--if-js"
+							type="button"
+						>
+							more info
+						</button>
+						<div className="o-expander__content">
+							{expandableContentHTML}
+						</div>
 					</div>
-				</div>
-			) : null}
-			{lockedBy ? (
-				<div className="o-forms__additional-info">
-					Not editable. Automatically populated by{' '}
-					<LinkToRecord type="System" value={{ code: lockedBy }} />.
-				</div>
-			) : null}
+				) : null}
+				{lockedBy ? (
+					<div className="o-forms__additional-info">
+						Not editable. Automatically populated by{' '}
+						<LinkToRecord
+							type="System"
+							value={{ code: lockedBy }}
+						/>
+						.
+					</div>
+				) : null}
+			</span>
 		</span>
-	</span>
-	)
+	);
 };
 
 const WrappedEditComponent = props => {
