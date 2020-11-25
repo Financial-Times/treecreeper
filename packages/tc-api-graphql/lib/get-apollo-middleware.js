20be9540dbb18230e3607e88a9d81c73f67f6059
Rhys Evans <wheresrhys@gmail.com>
2020-11-25 19:00:03 +0000

    encapsulate the document data loader better

--- packages/tc-api-graphql/lib/get-apollo-middleware.js
+++ packages/tc-api-graphql/lib/get-apollo-middleware.js
@@ -1,5 +1,4 @@
 const { ApolloServer } = require('apollo-server-express');
-const DataLoader = require('dataloader');
 const {
 	logger,
 	getContextByRequestId,
@@ -8,20 +7,10 @@ const { driver } = require('@financial-times/tc-api-db-manager');
 const { getAugmentedSchema } = require('./get-augmented-schema');
 const { Tracer } = require('./request-tracer');
 
-const getApolloMiddleware = ({
-	documentStore,
-	typeDefs,
-	resolvers,
-	excludeTypes,
-}) => {
+const getApolloMiddleware = options => {
 	const apollo = new ApolloServer({
 		subscriptions: false,
-		schema: getAugmentedSchema({
-			documentStore,
-			typeDefs,
-			resolvers,
-			excludeTypes,
-		}),
+		schema: getAugmentedSchema(options),
 		context: ({
 			req: { headers },
 			res: {
@@ -33,16 +22,6 @@ const getApolloMiddleware = ({
 				headers,
 				trace: new Tracer(getContextByRequestId(requestId)),
 			};
-
-			if (documentStore) {
-				context.documentStoreDataLoader = new DataLoader(async keys => {
-					const responses = await Promise.all(
-						keys.map(key => documentStore.get(...key.split('/'))),
-					);
-					return responses.map(({ body }) => body);
-				});
-			}
-
 			return context;
 		},
 		introspection: true,
--- packages/tc-api-graphql/lib/get-augmented-schema.js
+++ packages/tc-api-graphql/lib/get-augmented-schema.js
@@ -1,4 +1,5 @@
 const logger = require('@financial-times/n-logger').default;
+const DataLoader = require('dataloader');
 const { makeAugmentedSchema } = require('neo4j-graphql-js');
 const { applyMiddleware } = require('graphql-middleware');
 const { parse } = require('graphql');
@@ -6,41 +7,53 @@ const { getGraphqlDefs, getTypes } = require('@financial-times/tc-schema-sdk');
 const merge = require('lodash.merge');
 const { middleware: requestTracer } = require('./request-tracer');
 
-const resolveDocumentProperty = async ({ code }, args, context, info) => {
-	if (!code) {
-		throw new Error(
-			'Must include code in body of query that requests any Document properties',
-		);
-	}
-	const key = `${info.parentType.name}/${code}`;
-	const record = await context.documentStoreDataLoader.load(key);
-	return record[info.fieldName];
-};
-
-const getDocumentResolvers = () => {
-	const typeResolvers = {};
-	const types = getTypes();
-	types.forEach(type => {
-		const nodeProperties = type.properties;
-		const documentResolvers = {};
-		Object.keys(nodeProperties).forEach(prop => {
-			if (nodeProperties[prop].type === 'Document') {
-				documentResolvers[prop] = resolveDocumentProperty;
-			}
-		});
-		if (Object.keys(documentResolvers).length) {
-			typeResolvers[type.name] = documentResolvers;
-		}
-	});
-	return typeResolvers;
-};
-
 const getAugmentedSchema = ({
 	documentStore,
 	typeDefs: extendedTypeDefs,
 	resolvers: extendedResolvers,
 	excludeTypes,
 }) => {
+	const getDocumentDataLoader = () =>
+		new DataLoader(async keys => {
+			const responses = await Promise.all(
+				keys.map(key => documentStore.get(...key.split('/'))),
+			);
+			return responses.map(({ body }) => body);
+		});
+
+	const resolveDocumentProperty = async ({ code }, args, context, info) => {
+		if (!code) {
+			throw new Error(
+				'Must include code in body of query that requests any Document properties',
+			);
+		}
+		const key = `${info.parentType.name}/${code}`;
+
+		context.documentStoreDataLoader =
+			context.documentStoreDataLoader || getDocumentDataLoader();
+
+		const record = await context.documentStoreDataLoader.load(key);
+		return record[info.fieldName];
+	};
+
+	const getDocumentResolvers = () => {
+		const typeResolvers = {};
+		const types = getTypes();
+		types.forEach(type => {
+			const nodeProperties = type.properties;
+			const documentResolvers = {};
+			Object.keys(nodeProperties).forEach(prop => {
+				if (nodeProperties[prop].type === 'Document') {
+					documentResolvers[prop] = resolveDocumentProperty;
+				}
+			});
+			if (Object.keys(documentResolvers).length) {
+				typeResolvers[type.name] = documentResolvers;
+			}
+		});
+		return typeResolvers;
+	};
+
 	const resolvers = documentStore ? getDocumentResolvers() : {};
 	const typeDefs = getGraphqlDefs();
 
