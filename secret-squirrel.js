9c6accf3a7a5d911fce89d0ed79326659e5343d7
Rhys Evans <wheresrhys@gmail.com>
2020-11-25 18:02:35 +0000

    remove beta secret squirrel

--- secret-squirrel.js
+++ secret-squirrel.js
@@ -2,27 +2,16 @@ module.exports = {
 	files: {
 		allow: [
 			'scripts/neo4j-plugins',
-			'scripts/neo4j-wait-for-start',
-			'packages/tc-schema-sdk/data-accessors/__tests__/fixtures/generated-schema.graphql'
+			'scripts/neo4j-wait-for-start'
 		],
 		allowOverrides: []
 	},
 	strings: {
 		deny: [],
 		denyOverrides: [
-			'dd4c5d856812e0fb8c705feeabffd754', //gist link in readme
-			'e4d77800-5cab-11e9-8713-8d0ea7485108', // image in readme
-			'support@graphenedb.com', // support email address from runbook
-			'd5deb97d-5fa2-45f2-99fa-cd155328320d', // uuid for biz-ops-api dashboard url
-			'dba88780-1a7d-11e9-9f6b-e867440c3985', // README.md:4
-			'32e6ad1a-210f-11e8-89cc-978cb917c4e7', // doc/MODEL.md:5
-			'396e2a98-1d16-11e8-87fd-7f7eb4f8a221', // doc/MODEL.md:11
-			'33cba7fc-1d19-11e8-8417-7c85b306fa17', // doc/MODEL.md:21
-			'd5deb97d-5fa2-45f2-99fa-cd155328320d', // doc/db-upgrade.md:67
-			'tech@lt\\.com', // scripts/load-testing/lib/generate/team.js:29
-			'email@example.com', // test/v2/node-patch.spec.js:1812|1839
-			'447559/71667873-c7c56680-2d5e-11ea-9e44-a0947997e18c', // treecreeper logo
-			'me@here.com' // example email address
+			'me@here\\.com', // docs/treecreeper-graphql.md:15
+			'populateCuriousParent1RelationshipFields', // packages/tc-ui/src/primitives/relationship/__tests__/e2e-annotate-rich-relationships.cyp.js:17|17|277|277, packages/tc-ui/src/test-helpers/cypress.js:424|424|467|467
+			'populateCuriousParent2RelationshipFields' // packages/tc-ui/src/primitives/relationship/__tests__/e2e-annotate-rich-relationships.cyp.js:18|18|286|286, packages/tc-ui/src/test-helpers/cypress.js:429|429|468|468
 		]
 	}
 };
