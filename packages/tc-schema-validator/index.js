80fb87c75abaa2460337d9eb2fdbe44e15a5f629
Rhys Evans <rhys.evans@ft.com>
2020-12-17 22:21:55 +0000

    removed old tests

--- packages/tc-schema-validator/index.js
+++
@@ -1,8 +0,0 @@
-/* global describe */
-describe('Schema validation', () => {
-	require('./tests/enums'); // eslint-disable-line global-require
-	require('./tests/graphql-defs'); // eslint-disable-line global-require
-	require('./tests/string-patterns'); // eslint-disable-line global-require
-	require('./tests/type-hierarchy'); // eslint-disable-line global-require
-	require('./tests/types'); // eslint-disable-line global-require
-});
