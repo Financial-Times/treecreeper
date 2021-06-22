7a0c5bca3f53f0e1f011eb77b25ae290a8fa8fcc
Rhys Evans <rhys.evans@ft.com>
2021-06-22 15:39:12 +0100

    tweaks to deal with regression in neo4j driver
    https://github.com/neo4j/neo4j-javascript-driver/issues/758

--- packages/tc-api-rest-handlers/__tests__/patch-properties.spec.js
+++ packages/tc-api-rest-handlers/__tests__/patch-properties.spec.js
@@ -1,4 +1,4 @@
-const neo4jTemporalTypes = require('neo4j-driver/lib/temporal-types');
+const { types: neo4jTemporalTypes } = require('neo4j-driver');
 const { setupMocks, neo4jTest } = require('../../../test-helpers');
 const { dbUnavailable } = require('../../../test-helpers/error-stubs');
 const { spyDbQuery } = require('../../../test-helpers/db-spies');
--- packages/tc-api-rest-handlers/lib/neo4j-type-conversion.js
+++ packages/tc-api-rest-handlers/lib/neo4j-type-conversion.js
@@ -2,8 +2,8 @@ const {
 	isDateTime,
 	isDate,
 	isTime,
-} = require('neo4j-driver/lib/temporal-types');
-const neo4jTemporalTypes = require('neo4j-driver/lib/temporal-types');
+	types: neo4jTemporalTypes,
+} = require('neo4j-driver');
 const { getType } = require('@financial-times/tc-schema-sdk');
 
 const entriesToObject = (map, [key, val]) => Object.assign(map, { [key]: val });
